94b45ddefa3c07b00806f7b42542e5cce08f6688
amurariu <amurariu@uwo.ca>
2025-05-06 11:20:00 -0400

    edit

--- PD1_draft.R
+++ PD1_draft.R
@@ -5,11 +5,12 @@ library(DESeq2,quietly=T)
 library(ALDEx2,quietly=T)
 #add ALDEx3
 
-readCount<-read.table(file="data/imm_GSE91061_raw_counts_GRCh38.p13_NCBI.tsv", header = T, skip=35, sep='\t', row.names = 1)
-meta <- read.table(file="data/metadata.txt", header=F, row.names=1, sep='\t') #new file with accession numbers and patient IDs
+readCount<-read.table(file="data/imm-GSE91061_raw_counts_GRCh38.p13_NCBI.tsv", header = T, skip=35, sep='\t', row.names = 1,)
 
-conditions <- rep("Pre", 109)
-grep("_On",meta)
-conditions[grep("_On",meta)] <- "On"
-conds <- data.frame(conditions)
-conds$condition <- factor(conds$condition)
+meta <- read.table(file="metadata.txt", header=F, row.names=1, sep="\t") #new file with accession numbers and patient IDs
+
+#conditions <- rep("Pre", 109)
+#grep("_On",meta)
+#conditions[grep("_On",meta)] <- "On"
+#conds <- data.frame(conditions)
+#conds$condition <- factor(conds$condition)
