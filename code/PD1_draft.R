e2402bd6fd73b7a918c5577af0bf56696c3e7955
amurariu <amurariu@uwo.ca>
2025-05-07 13:05:39 -0400

    wd

--- code/PD1_draft.R
+++ code/PD1_draft.R
@@ -1,10 +1,11 @@
 #setwd("/home/amurariu/Documents/github/usri/")
-setwd("/home/amurariu/usri")
+#setwd("/home/amurariu/usri")
 
-immuno<-read.table(file="data/imm-GSE91061_raw_counts_GRCh38.p13_NCBI.tsv", header = T, skip=35, sep='\t', row.names = 1)
-m <- read.table(file="data/imm_metadata.txt", header=F, row.names=1, sep='\t')
+url <- "https://rawgithubusercontent.com/amurariu/usri/main/data/imm-GSE91061_raw_counts_GRCh38.p13_NCBI.tsv"
+meta <- "https://rawgithubusercontent.com/amurariu/usri/main/data/imm_metadata.txt"
 
-##stopped editing here, start here
+immuno<-read.table(file=url, header = T, skip=35, sep='\t', row.names = 1)
+m <- read.table(file=meta, header=F, row.names=1, sep='\t')
 
 if(file.exists("analysis/thin_sim_data.out.Rda")){
   load("analysis/thin_sim_data.out.Rda") # file is data.out, list
@@ -37,20 +38,51 @@ if(file.exists("analysis/thin_sim_data.out.Rda")){
     thin.immuno <- thin_2group(immuno.data, prop_null=0.95, alpha=0,
                                signal_fun = stats::rnorm, signal_params = list(mean = 0, sd = 2))
     
-    x <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=1e-3)
-    x.2 <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=0.2)
-    x.5 <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=0.5)
+    #x <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=1e-3)
+   # x.2 <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=0.2)
+    #x.5 <- aldex(thin.immuno$mat, conditions=as.vector(thin.immuno$designmat), gamma=0.5)
     
     conds <- as.vector(thin.immuno$designmat)
     
     #DESeq2 functions
+    setClassUnion("ExpData", c("matrix", "SummarizedExperiment")) #added due to error message being shown for DESeq2
     dds.th  <- DESeqDataSetFromMatrix(countData = thin.immuno$mat,
                                       colData = data.frame(conds),
                                       design = ~ conds)
     dds.th <- DESeq(dds.th)
     res.th <- results(dds.th)
-    data.iter <- list(coef=thin.immuno$coefmat, ald0=x, ald2=x.2, ald5=x.5, des=res.th)
-    data.out[[i]] <- data.iter
+    
+    
+    #EdgeR Code
+      immuno.conds$conditions <- factor(immuno.conds$conditions)
+      design <- model.matrix(~group)
+      
+      y <- calcNormFactors(y)
+      count_norm = cpm(y)
+      y <- estimateDisp(y,design)
+      fit <- glmQLFit(y,design)
+      qlf <- glmQLFTest(fit,coef=2)
+      qlf_i = topTags(qlf, n = nrow(conditions), p.value = 1)@.Data[[1]]
+      pvalues = qlf_i[match(rownames(count1), rownames(qlf_i)),"PValue"]
+      qlf <- topTags(qlf, n = nrow(dat), p.value = q)
+      discovery = rownames(qlf)
+      output <- list(discovery, pvalues)
+      return(output)
+    
+    
+    
+    
+    #data.iter <- list(coef=thin.immuno$coefmat, ald0=x, ald2=x.2, ald5=x.5, des=res.th)
+    #data.out[[i]] <- data.iter
   }
-  save(data.out, file="../analysis/thin_sim_data_draft.out.Rda")
+  #save(data.out, file="../analysis/thin_sim_data_draft.out.Rda")
 }
+
+
+
+#design <- model.matrix(~conds)
+# fit <- glmQLFit(y, design)
+# qlf <- glmQLFit(fit, coef=2) # returns error message because outputs negative values, which should not be possible in reality (ie. gene expression levels should not be negative values)
+# 
+# count_norm=cpm(y) 
+# count_norm<-as.data.frame(count_norm) 
