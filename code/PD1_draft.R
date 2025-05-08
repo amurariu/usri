2d79590020574c772de8177b9fee35c534a3f428
amurariu <amurariu@uwo.ca>
2025-05-08 10:27:52 -0400

    edit

--- code/PD1_draft.R
+++ code/PD1_draft.R
@@ -32,11 +32,11 @@ if(file.exists("analysis/thin_sim_data.out.Rda")){
                                     colData = immuno.conds,
                                     design = ~ conditions)
   dds.u <- DESeq(dds.u)
-  res.u <- results(dds.u)}
+  res.u <- results(dds.u)
   
   #randomized + TP addition for DESeq2
+  
   # do 10 replicates and keep outputs
-  for(i in 1:10){
     # this adds rnorm noise to 5% of the transcripts
     # setting alpha=1 gives no difference between if features are
     # approximately gaussian
@@ -45,25 +45,24 @@ if(file.exists("analysis/thin_sim_data.out.Rda")){
     
     condsp <- as.vector(thin.immuno$designmat)
     
-    setClassUnion("ExpData", c("matrix", "SummarizedExperiment")) #added due to error message being shown for DESeq2
+   # setClassUnion("ExpData", c("matrix", "SummarizedExperiment")) #added due to error message being shown for DESeq2, sometimes works and sometimes doesn't?
     dds.th  <- DESeqDataSetFromMatrix(countData = thin.immuno$mat,
                                       colData = data.frame(condsp),
                                       design = ~ condsp)
     dds.th <- DESeq(dds.th)
-    res.th <- results(dds.th)}
+    res.th <- results(dds.th)
     
     
 #unpermuted edgeR
-  for (i in 1:10){
   group<-factor(conditions)
   design <- model.matrix(~group)
   fit <- glmQLFit(y,design)
   qlf <- glmQLFTest(fit,coef=2)
-  edgeR.res.u<-topTags(qlf, n=20478, adjust.method = "BH", sort.by = "none", p.value = 1)}
+  edgeR.res.u<-topTags(qlf, n=20478, adjust.method = "BH", sort.by = "none", p.value = 1)
 
   
 #randomized + TP addition edgeR  
-  for (i in 1:10){
+  
     thin.immuno_e <- thin_2group(immuno.data, prop_null=0.95, alpha=0,
                                signal_fun = stats::rnorm, signal_params = list(mean = 0, sd = 2)) #confirm if want to use the same thin.immuno group or no
     cond_e <- as.vector(thin.immuno_e$designmat)
@@ -73,12 +72,12 @@ if(file.exists("analysis/thin_sim_data.out.Rda")){
       fit <- glmQLFit(thin.immuno_e,design_e) #negative counts not allowed message
       
       qlf <- glmQLFTest(fit,coef=2)
-      edgeR.res.p<-topTags(qlf, n=20478, adjust.method = "BH", sort.by = "none", p.value = 1)}
+      edgeR.res.p<-topTags(qlf, n=20478, adjust.method = "BH", sort.by = "none", p.value = 1)
       
-    #data.iter <- list(coef=thin.immuno$coefmat, ald0=x, ald2=x.2, ald5=x.5, des=res.th)
-    #data.out[[i]] <- data.iter
+    data.iter <- list(coef=thin.immuno$coefmat, ald0=x, ald2=x.2, ald5=x.5, des=res.th)
+    data.out[[i]] <- data.iter
   }
-  #save(data.out, file="../analysis/thin_sim_data_draft.out.Rda")
+  save(data.out, file="../analysis/thin_sim_data_draft.out.Rda")
 }
 
 #create for loop such that it repeats the whole thing and adds it into one file instead of 4 loops
