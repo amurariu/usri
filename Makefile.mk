ca9027f017f8be61b805a21365d6a0effd16faa3
amurariu <amurariu@uwo.ca>
2025-05-09 12:17:21 -0400

    update

--- Makefile.mk
+++ Makefile.mk
@@ -1,32 +1,13 @@
 #makes the file .Rda if make all is called
-#all: analysis/test2.Rda
-
-SCRIPT_DIR = code
-ANALYSIS_DIR = analysis
-
-SCRIPT = $(SCRIPT_DIR)/brca_draft.R
-ANALYSIS = $(ANALYSIS_DIR)/test2.Rda
-
-
-#target
-all: $(ANALYSIS)
-
-#generating output
-$(OUTPUT): $(SCRIPT)
-	mkdir -p $(ANALYSIS_DIR)
-	Rscript $(SCRIPT) > $(ANALYSIS)
-
-clean:
-	rm -f $(OUTPUT)
-
+all: analysis/test2.Rda
 
 #target is BRCA_Draft.Rda
 #prereq is brca_draft.R
 #recipe is to run the R script
 
-#analysis/test2.Rda: 'code/brca_draft.R'
-#	Rscript 'code/brca_draft.R'
+analysis/test2.Rda: 'code/brca_draft.R'
+	Rscript 'code/brca_draft.R'
 
 #clean:	
-#	rm analysis/test2.Rda
+	rm analysis/test2.Rda
 	
