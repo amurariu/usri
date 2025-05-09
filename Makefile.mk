254736d9ab9af35e90e6fde5859359c398a4b326
amurariu <amurariu@uwo.ca>
2025-05-09 11:40:52 -0400

    Update Makefile

--- Makefile.mk
+++ Makefile.mk
@@ -1,12 +1,12 @@
 #makes the file x.Rda if make all is called
-all: BRCA_Draft.Rda
+all: BRCA_draft.Rda
 
 #target is BRCA_Draft.Rda
 #prereq is brca_draft.R
 #recipe is to run the R script
 
-x.Rda: brca_draft.R
+BRCA_draft.Rda: brca_draft.R
 	Rscript 'brca_draft.R'
 
-clean:
-	rm BRCA_Draft.Rda
+#clean:	
+#rm BRCA_Draft.Rda
