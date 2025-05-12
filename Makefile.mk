049ee56bcfedda034823d43eb152de79bf295e61
amurariu <amurariu@uwo.ca>
2025-05-12 14:22:34 -0400

    check

--- Makefile.mk
+++ Makefile.mk
@@ -1,13 +1,14 @@
 #makes the file .Rda if make all is called
-all: analysis/test2.Rda
+#all: analysis/test2.Rda
 
+all: analysis/immuno.data.u.deseq.Rda
 #target is BRCA_Draft.Rda
 #prereq is brca_draft.R
 #recipe is to run the R script
 
 #rules to generate the deseq output files
-#analysis/immuno.data.u.deseq.Rda: code/deseq.R
-#	Rscript 'code/deseq.R'
+analysis/immuno.data.u.deseq.Rda: code/deseq.R
+	Rscript 'code/deseq.R'
 	
 #analysis/immuno.data.r.deseq.Rda: code/deseq.R
 #	Rscript 'code/deseq.R'
@@ -24,14 +25,14 @@ all: analysis/test2.Rda
 #analysis/brca.data.p.deseq.Rda: code/deseq.R
 #	Rscript 'code/deseq.R'
 	
-analysis/test2.Rda: code/brca_draft.R
-	Rscript 'code/brca_draft.R'
+#analysis/test2.Rda: code/brca_draft.R
+#	Rscript 'code/brca_draft.R'
 
 clean:	
-	rm analysis/test2.Rda
+	#rm analysis/test2.Rda
 	
 	
-	#rm analysis/immuno.data.u.deseq.Rda
+	rm analysis/immuno.data.u.deseq.Rda
 	
 #	rm analysis/immuno.data.r.deseq.Rda
 	
