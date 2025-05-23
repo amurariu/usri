eb87847e9763937176b0347e54586b75b300b4d3
amurariu <amurariu@uwo.ca>
2025-05-23 15:18:22 -0400

    Update ald.fun.R

--- code/ald.fun.R
+++ code/ald.fun.R
@@ -23,43 +23,25 @@ ald.fun <- function(data, conditions, nloop=4){
     thin <- thin_2group(immuno.data, prop_null=0.95, alpha=0,
                         signal_fun = stats::rnorm, 
                         signal_params = list(mean = 0, sd = 2))
-    thin.data.out.edger[[i]] <- thin
+    thin.data.out.aldex[[i]] <- thin
     condsp <- as.vector(thin$designmat)   # permuted and thinned conditions and data
     datasp <- thin$mat
     
-    #edgeR analysis
-    #PD1 setup
-    group_p <- factor(condsp)
-    design_p <- model.matrix(~group_p) #use data randomization from seqgendiff
-    
     #randomized without FP addition PD1
-    fit_rp <- glmQLFit(immuno.data,design_p) #uses original data (ie. no TP added)
-    qlf_rp <- glmQLFTest(fit_rp,coef=2)
-    edg.rp<-topTags(qlf_rp, n=nrow(immuno.data), adjust.method = "BH", sort.by = "none", p.value = 1)
-    
-    resrp.edgeR<-list(resu=edg.rp)
-    data.out.edgeR.r[[i]] <- as.data.frame(resrp.edgeR)
+    xrp.aldex <- aldex(immuno.data, conditions=condsp, gamma=1e-3) #uses original dataset but permuted conditions
+    resrp.aldex<-list(resu=xrp.aldex)
+    data.out.aldex.r[[i]] <- as.data.frame(resrp.aldex)
     
     #randomized with FP addition PD1
-    fit_pp <- glmQLFit(datasp,design_p)
-    qlf_pp <- glmQLFTest(fit_pp,coef=2)
-    edg.pp<-topTags(qlf_pp, n=nrow(datasp), adjust.method = "BH", sort.by = "none", p.value = 1)
-    
-    respp.edgeR<-list(resu=edg.pp)
-    data.out.edgeR.p[[i]] <- as.data.frame(respp.edgeR)
+    xpp.aldex <- aldex(datasp, conditions=condsp, gamma=1e-3) #uses new dataset with permuted conditions
+    respp.aldex<-list(resu=xpp.aldex)
+    data.out.aldex.p[[i]] <- as.data.frame(respp.aldex)
   }
   print("done loop")
   
-  
   #unpermuted PD1
-  group_up<-factor(conditions_p)
-  design_up <- model.matrix(~group_up)
-  fit_up <- glmQLFit(y_pd1,design_up)
-  qlf_up <- glmQLFTest(fit_up,coef=2)
-  edg.up<-topTags(qlf_up, n=nrow(immuno.data), adjust.method = "BH", sort.by = "none", p.value = 1) 
+  xup.aldex <- aldex(immuno.data, conditions=immuno.conds, gamma=1e-3)
+  immuno.data.out.aldex.u <- list(xup.aldex)
   
-  resup.edgeR<-list(resu=edg.up)
-  data.out.edgeR.u <- list(resup.edgeR)
-  
-  return(list(conditions=conditions_p, thin.data=thin.data.out.edger, u.data=data.out.edgeR.u, r.data=data.out.edgeR.r, p.data=data.out.edgeR.p))
+  return(list(conditions=conditions_p, thin.data=thin.data.out.aldex, u.data=data.out.aldex.u, r.data=data.out.aldex.r, p.data=data.out.aldex.p))
 }
