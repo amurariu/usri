95552f70ad17336cef96dd1c86877da385a90d70
amurariu <amurariu@uwo.ca>
2025-05-30 15:42:01 -0400

    change #

--- code/edg.fun.R
+++ code/edg.fun.R
@@ -2,7 +2,7 @@
 # conditions is conditions_p from above
 # name is the name of the output file and must be in quotes
 # nloops is the number of test loops
-edg.fun <- function(data, conditions, nloop=1){
+edg.fun <- function(data, conditions, nloop=100){
   #assign(paste("perf.a", "1", sep=""),5)
   #perf.a1
   conditions_p <- conditions
