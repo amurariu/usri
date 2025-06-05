aa5d30e70612c30626eeadda6d20d18aa4a379da
amurariu <amurariu@uwo.ca>
2025-06-05 10:02:16 -0400

    analysis file for function

--- code/analysis.R
+++ code/analysis.R
@@ -1,7 +1,17 @@
 anal.path <- "../ext_analysis/" #move ext_analysis back to github folder
 
+source('code/immuno_TPFP.R')
+
+#load files
+#immuno dataset
 load(paste(anal.path,"immuno.data.deseq.Rda", sep="")) 
 load(paste(anal.path,"immuno.data.edger.Rda", sep=""))
+load(paste(anal.path,"immuno.data.aldex2_0.out.Rda", sep=""))
+load(paste(anal.path,"immuno.data.aldex2_2.out.Rda", sep=""))
+load(paste(anal.path,"immuno.data.aldex2_5.out.Rda", sep=""))
 
+#inputting parameters for function
+immuno.analysis <- analysis.fun(analysis.deseq = immuno.data.DESeq, analysis.edgeR = immuno.data.edgeR, analysis.aldex0 = immuno.data_0.aldex2, analysis.aldex0.2 = immuno.data_2.aldex2, analysis.aldex0.5 = immuno.data_5.aldex2) 
+save(immuno.analysis, file="./analysis/immuno.data.analysis.Rda") #should it be an Rda file or another file type
 
-#
+  
