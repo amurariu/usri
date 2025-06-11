426460e4faef7669f419ee91efdb4ec871deb902
amurariu <amurariu@uwo.ca>
2025-06-11 16:59:03 -0400

    Update immuno_TPFP.R

--- code/immuno_TPFP.R
+++ code/immuno_TPFP.R
@@ -30,11 +30,6 @@ analysis.fun <- function(input=NULL, type=NULL, nloop=100) {
   }
 
 #if (input == NULL) {stop("input modelled data")} #says to use is.null?
-    
-# equivalence
-# immuno.data_0.aldex2$p.data[[i]]$we.eBH == data.out[[i]]$ald0$wi.eBH
-# immuno.data.DESeq$p.data[[i]]$padj == data.out[[i]]$des0$padj
-#aldex.0 <- immuno.data_0.aldex2
 
 	coeff.vec <- c(0.01,0.1,0.2,0.5,0.75,1)
 		
@@ -51,10 +46,41 @@ analysis.fun <- function(input=NULL, type=NULL, nloop=100) {
 	SPE.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
 	SPE.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
 	
+	PPV.LFC.0.5.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	FDR.LFC.0.5.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SEN.LFC.0.5.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SPE.LFC.0.5.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	PPV.LFC.1.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	FDR.LFC.1.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SEN.LFC.1.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SPE.LFC.1.r <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	PPV.LFC.0.5.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	PPV.LFC.0.5.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	FDR.LFC.0.5.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	FDR.LFC.0.5.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	SEN.LFC.0.5.p <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	SPE.LFC.0.5.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SPE.LFC.0.5.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	PPV.LFC.1.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	PPV.LFC.1.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	FDR.LFC.1.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	FDR.LFC.1.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	SEN.LFC.1.p <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
+	SPE.LFC.1.pp <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	SPE.LFC.1.pr <- matrix(data=NA, nrow=nloop, ncol=length(coeff.vec))
+	
 	for(coeff in 1:6){
 			for(i in 1:nloop){ 
-			
-			
+		 # print(c(coeff, i))
 		# coefficients same in every instance
 		# can hardcode this if sanity check passes
 		
@@ -108,6 +134,17 @@ analysis.fun <- function(input=NULL, type=NULL, nloop=100) {
 		SEN.r[i, coeff] <- length(TP.r)/(length(TP.r) + length(FN.r))
 		SPE.r[i, coeff] <- length(TN.r)/(length(TN.r) + length(FP.r))
 		
+		PPV.LFC.0.5.r[i, coeff] <- length(TP.LFC.0.5.r)/sum(length(TP.LFC.0.5.r),length(FP.LFC.0.5.r))
+		FDR.LFC.0.5.r[i, coeff] <- length(FP.LFC.0.5.r)/sum(length(TP.LFC.0.5.r),length(FP.LFC.0.5.r))
+		SEN.LFC.0.5.r[i, coeff] <- length(TP.LFC.0.5.r)/(length(TP.LFC.0.5.r) + length(FN.LFC.0.5.r))
+		SPE.LFC.0.5.r[i, coeff] <- length(TN.LFC.0.5.r)/(length(TN.LFC.0.5.r) + length(FP.LFC.0.5.r))
+		
+		PPV.LFC.1.r[i, coeff] <- length(TP.LFC.1.r)/sum(length(TP.LFC.1.r),length(FP.LFC.1.r))
+		FDR.LFC.1.r[i, coeff] <- length(FP.LFC.1.r)/sum(length(TP.LFC.1.r),length(FP.LFC.1.r))
+		SEN.LFC.1.r[i, coeff] <- length(TP.LFC.1.r)/(length(TP.LFC.1.r) + length(FN.LFC.1.r))
+		SPE.LFC.1.r[i, coeff] <- length(TN.LFC.1.r)/(length(TN.LFC.1.r) + length(FP.LFC.1.r))
+		
+		
 		#for randomized + permuted data (p)
 		PPV.pr[i, coeff] <- length(TP.p)/sum(length(TP.p),length(FP.pr))
 		PPV.pp[i, coeff] <- length(TP.p)/sum(length(TP.p),length(FP.pp))
@@ -117,185 +154,209 @@ analysis.fun <- function(input=NULL, type=NULL, nloop=100) {
 		SPE.pr[i, coeff] <- length(TN.pr)/(length(TN.pr) + length(FP.pr))
 		SPE.pp[i, coeff] <- length(TN.pp)/(length(TN.pp) + length(FP.pp))
 		
-		return(list(PPV.r, FDR.r, SEN.r, SPE.r, PPV.pr,  PPV.pp, FDR.pr, FDR.pp, SEN.p, SPE.pr = SPE.pr, SPE.pp = SPE.pp))
+		PPV.LFC.0.5.pp[i, coeff] <- length(TP.LFC.0.5.p)/sum(length(TP.LFC.0.5.p),length(FP.LFC.0.5.pp))
+		PPV.LFC.0.5.pr[i, coeff] <- length(TP.LFC.0.5.p)/sum(length(TP.LFC.0.5.p),length(FP.LFC.0.5.pr))
+		
+		FDR.LFC.0.5.pp[i, coeff] <- length(FP.LFC.0.5.pp)/sum(length(TP.LFC.0.5.p),length(FP.LFC.0.5.pp))
+		FDR.LFC.0.5.pr[i, coeff] <- length(FP.LFC.0.5.pr)/sum(length(TP.LFC.0.5.p),length(FP.LFC.0.5.pr))
+		
+		SEN.LFC.0.5.p[i, coeff] <- length(TP.LFC.0.5.p)/(length(TP.LFC.0.5.p) + length(FN.LFC.0.5.p))
+		
+		SPE.LFC.0.5.pp[i, coeff] <- length(TN.LFC.0.5.pp)/(length(TN.LFC.0.5.pp) + length(FP.LFC.0.5.pp))
+		SPE.LFC.0.5.pr[i, coeff] <- length(TN.LFC.0.5.pr)/(length(TN.LFC.0.5.pr) + length(FP.LFC.0.5.pr))
+		
+		
+		PPV.LFC.1.pp[i, coeff] <- length(TP.LFC.1.p)/sum(length(TP.LFC.1.p),length(FP.LFC.1.pp))
+		PPV.LFC.1.pr[i, coeff] <- length(TP.LFC.1.p)/sum(length(TP.LFC.1.p),length(FP.LFC.1.pr))
+		
+		FDR.LFC.1.pp[i, coeff] <- length(FP.LFC.1.pp)/sum(length(TP.LFC.1.p),length(FP.LFC.1.pp))
+		FDR.LFC.1.pr[i, coeff] <- length(FP.LFC.1.pr)/sum(length(TP.LFC.1.p),length(FP.LFC.1.pr))
+		
+		SEN.LFC.1.p[i, coeff] <- length(TP.LFC.1.p)/(length(TP.LFC.1.p) + length(FN.LFC.1.p))
+		
+		SPE.LFC.1.pp[i, coeff] <- length(TN.LFC.1.pp)/(length(TN.LFC.1.pp) + length(FP.LFC.1.pp))
+		SPE.LFC.1.pr[i, coeff] <- length(TN.LFC.1.pr)/(length(TN.LFC.1.pr) + length(FP.LFC.1.pr))
+
+
 		} # end coeff loop
 	} # end randomization loop
+	return(list(PPV.r, FDR.r, SEN.r, SPE.r, PPV.pr,  PPV.pp, FDR.pr, FDR.pp, SEN.p, SPE.pr, SPE.pp, PPV.LFC.0.5.r, FDR.LFC.0.5.r, SPE.LFC.0.5.r, SEN.LFC.0.5.r, PPV.LFC.1.r, FDR.LFC.1.r, SPE.LFC.1.r, SEN.LFC.1.r, PPV.LFC.0.5.pp, PPV.LFC.0.5.pr, FDR.LFC.0.5.pp, FDR.LFC.0.5.pr, SEN.LFC.0.5.p, SPE.LFC.0.5.pp, SPE.LFC.0.5.pr, PPV.LFC.1.pp, PPV.LFC.1.pr, FDR.LFC.1.pp, FDR.LFC.1.pr, SEN.LFC.1.p, SPE.LFC.1.pp, SPE.LFC.1.pr))
 } #end function 
 # finished editing up to here
-
-# even butt uglier ...
-for (j in 1:5){
-  analysis.wi[row,1] <- coeff
-  analysis.wi[row,2] <- i
-if (j==1){
-  analysis.wi[row,4] <- PPV.wi.ald
-  analysis.wi[row,5] <- FDR.wi.ald
-  analysis.wi[row,6] <- SEN.wi.ald
-  analysis.wi[row,7] <- SPE.wi.ald
-} else if (j==2) {
-  analysis.wi[row,4] <- PPV.wi.ald.2
-  analysis.wi[row,5] <- FDR.wi.ald.2
-  analysis.wi[row,6] <- SEN.wi.ald.2
-  analysis.wi[row,7] <- SPE.wi.ald.2
-} else if (j==3){
-  analysis.wi[row,4] <- PPV.wi.ald.5
-  analysis.wi[row,5] <- FDR.wi.ald.5
-  analysis.wi[row,6] <- SEN.wi.ald.5
-  analysis.wi[row,7] <- SPE.wi.ald.5
-} else if (j==4){
-  analysis.wi[row,4] <- PPV.wi.des
-  analysis.wi[row,5] <- FDR.wi.des
-  analysis.wi[row,6] <- SEN.wi.des
-  analysis.wi[row,7] <- SPE.wi.des
-} else if (j==5){
-  analysis.wi[row,4] <- PPV.wi.des5
-  analysis.wi[row,5] <- FDR.wi.des5
-  analysis.wi[row,6] <- SEN.wi.des5
-  analysis.wi[row,7] <- SPE.wi.des5
-}
-row=row+1
-}
-}
-}
-
-means.wi <- matrix(data=NA, nrow=30, ncol=6)
-means.wi <- as.data.frame(means.wi)
-colnames(means.wi) <- c("coef", "method", "PPV","FDR","SEN","SPE")
-
-# don't do this!!!!!!
-# ugly hack
-for(i in 1:5){
-  means.wi[i,1:2] <- analysis.wi[seq(from=i, to=50, by=5),c(1,3)][1,]
-  means.wi[i,3:6] <- colMeans(analysis.wi[seq(from=i, to=50, by=5),4:7])
-}
-for(i in 1:5){
-  means.wi[i+5,1:2] <- analysis.wi[seq(from=i+50, to=100, by=5),c(1,3)][1,]
-  means.wi[i+5,3:6] <- colMeans(analysis.wi[seq(from=i+50, to=100, by=5),4:7])
-}
-for(i in 1:5){
-  means.wi[i+10,1:2] <- analysis.wi[seq(from=i+100, to=150, by=5),c(1,3)][1,]
-  means.wi[i+10,3:6] <- colMeans(analysis.wi[seq(from=i+100, to=150, by=5),4:7])
-}
-for(i in 1:5){
-  means.wi[i+15,1:2] <- analysis.wi[seq(from=i+150, to=200, by=5),c(1,3)][1,]
-  means.wi[i+15,3:6] <- colMeans(analysis.wi[seq(from=i+150, to=200, by=5),4:7])
-}
-for(i in 1:5){
-  means.wi[i+20,1:2] <- analysis.wi[seq(from=i+201, to=250, by=5),c(1,3)][1,]
-  means.wi[i+20,3:6] <- colMeans(analysis.wi[seq(from=i+201, to=250, by=5),4:7])
-}
-for(i in 1:5){
-  means.wi[i+25,1:2] <- analysis.wi[seq(from=i+251, to=300, by=5),c(1,3)][1,]
-  means.wi[i+25,3:6] <- colMeans(analysis.wi[seq(from=i+251, to=300, by=5),4:7])
-}
-
-FN.ald <- setdiff(model, which(data.out[[i]]$ald0$we.eBH < 0.05))
-FN.ald.5 <- setdiff(model, which(data.out[[i]]$ald5$we.eBH < 0.05))
-FN.ald.2 <- setdiff(model, which(data.out[[i]]$ald2$we.eBH < 0.05))
-FN.des <- setdiff(model, which(data.out[[i]]$des$padj < 0.05))
-FN.des5 <- setdiff(model, which(data.out[[i]]$des$padj < 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5))
-
-FP.ald <- intersect(which(data.out[[i]]$ald0$we.eBH < 0.05), null.model)
-FP.ald.2 <- intersect(which(data.out[[i]]$ald2$we.eBH < 0.05), null.model)
-FP.ald.5 <- intersect(which(data.out[[i]]$ald5$we.eBH < 0.05), null.model)
-FP.des <- intersect(which(data.out[[i]]$des$padj < 0.05), null.model)
-FP.des5 <- intersect(which(data.out[[i]]$des$padj < 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5), null.model)
-
-
-TN.des <- intersect(which(data.out[[i]]$des$padj >= 0.05), null.model)
-TN.des5 <- intersect(which(data.out[[i]]$des$padj >= 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5), null.model)
-
-PPV.ald <- length(TP.ald)/sum(length(TP.ald),length(FP.ald))
-PPV.ald.2 <- length(TP.ald.2)/sum(length(TP.ald.2),length(FP.ald.2))
-PPV.ald.5 <- length(TP.ald.5)/sum(length(TP.ald.5),length(FP.ald.5))
-PPV.des <- length(TP.des)/sum(length(TP.des),length(FP.des))
-PPV.des5 <- length(TP.des5)/sum(length(TP.des5),length(FP.des5))
-
-FDR.ald <- length(FP.ald)/sum(length(TP.ald),length(FP.ald))
-FDR.ald.2 <- length(FP.ald.2)/sum(length(TP.ald.2),length(FP.ald.2))
-FDR.ald.5 <- length(FP.ald.5)/sum(length(TP.ald.5),length(FP.ald.5))
-FDR.des <- length(FP.des)/sum(length(TP.des),length(FP.des))
-FDR.des5 <- length(FP.des5)/sum(length(TP.des5),length(FP.des5))
-
-SEN.ald <- length(TP.ald)/(length(TP.ald) + length(FN.ald))
-SEN.ald.2 <- length(TP.ald.2)/(length(TP.ald.2) + length(FN.ald.2))
-SEN.ald.5 <- length(TP.ald.5)/(length(TP.ald.5) + length(FN.ald.5))
-SEN.des <- length(TP.des)/(length(TP.des) + length(FN.des))
-SEN.des5 <- length(TP.des5)/(length(TP.des5) + length(FN.des5))
-
-SPE.ald <- length(TN.ald)/(length(TN.ald) + length(FP.ald))
-SPE.ald.2 <- length(TN.ald.2)/(length(TN.ald.2) + length(FP.ald.2))
-SPE.ald.5 <- length(TN.ald.5)/(length(TN.ald.5) + length(FP.ald.5))
-SPE.des <- length(TN.des)/(length(TN.des) + length(FP.des))
-SPE.des5 <- length(TN.des5)/(length(TN.des5) + length(FP.des5))
-
-# even butt uglier ...
-for (j in 1:5){
-  analysis.out[row,1] <- coeff
-  analysis.out[row,2] <- i
-if (j==1){
-  analysis.out[row,4] <- PPV.ald
-  analysis.out[row,5] <- FDR.ald
-  analysis.out[row,6] <- SEN.ald
-  analysis.out[row,7] <- SPE.ald
-} else if (j==2) {
-  analysis.out[row,4] <- PPV.ald.2
-  analysis.out[row,5] <- FDR.ald.2
-  analysis.out[row,6] <- SEN.ald.2
-  analysis.out[row,7] <- SPE.ald.2
-} else if (j==3){
-  analysis.out[row,4] <- PPV.ald.5
-  analysis.out[row,5] <- FDR.ald.5
-  analysis.out[row,6] <- SEN.ald.5
-  analysis.out[row,7] <- SPE.ald.5
-} else if (j==4){
-  analysis.out[row,4] <- PPV.des
-  analysis.out[row,5] <- FDR.des
-  analysis.out[row,6] <- SEN.des
-  analysis.out[row,7] <- SPE.des
-} else if (j==5){
-  analysis.out[row,4] <- PPV.des5
-  analysis.out[row,5] <- FDR.des5
-  analysis.out[row,6] <- SEN.des5
-  analysis.out[row,7] <- SPE.des5
-}
-row=row+1
-}
-}
-}
-
-means <- matrix(data=NA, nrow=30, ncol=6)
-means <- as.data.frame(means)
-colnames(means) <- c("coef", "method", "PPV","FDR","SEN","SPE")
-
-for(i in 1:5){
-  means[i,1:2] <- analysis.out[seq(from=i, to=50, by=5),c(1,3)][1,]
-  means[i,3:6] <- colMeans(analysis.out[seq(from=i, to=50, by=5),4:7])
-}
-for(i in 1:5){
-  means[i+5,1:2] <- analysis.out[seq(from=i+50, to=100, by=5),c(1,3)][1,]
-  means[i+5,3:6] <- colMeans(analysis.out[seq(from=i+50, to=100, by=5),4:7])
-}
-for(i in 1:5){
-  means[i+10,1:2] <- analysis.out[seq(from=i+100, to=150, by=5),c(1,3)][1,]
-  means[i+10,3:6] <- colMeans(analysis.out[seq(from=i+100, to=150, by=5),4:7])
-}
-for(i in 1:5){
-  means[i+15,1:2] <- analysis.out[seq(from=i+150, to=200, by=5),c(1,3)][1,]
-  means[i+15,3:6] <- colMeans(analysis.out[seq(from=i+150, to=200, by=5),4:7])
-}
-for(i in 1:5){
-  means[i+20,1:2] <- analysis.out[seq(from=i+201, to=250, by=5),c(1,3)][1,]
-  means[i+20,3:6] <- colMeans(analysis.out[seq(from=i+201, to=250, by=5),4:7])
-}
-for(i in 1:5){
-  means[i+25,1:2] <- analysis.out[seq(from=i+251, to=300, by=5),c(1,3)][1,]
-  means[i+25,3:6] <- colMeans(analysis.out[seq(from=i+251, to=300, by=5),4:7])
-}
-
-
-ald.row <- which(means$method == "ald")
-ald2.row <- which(means$method == "ald2")
-ald5.row <- which(means$method == "ald5")
-des.row <- which(means$method == "des")
-des5.row <- which(means$method == "des5")
+# 
+# # even butt uglier ...
+# for (j in 1:5){
+#   analysis.wi[row,1] <- coeff
+#   analysis.wi[row,2] <- i
+# if (j==1){
+#   analysis.wi[row,4] <- PPV.wi.ald
+#   analysis.wi[row,5] <- FDR.wi.ald
+#   analysis.wi[row,6] <- SEN.wi.ald
+#   analysis.wi[row,7] <- SPE.wi.ald
+# } else if (j==2) {
+#   analysis.wi[row,4] <- PPV.wi.ald.2
+#   analysis.wi[row,5] <- FDR.wi.ald.2
+#   analysis.wi[row,6] <- SEN.wi.ald.2
+#   analysis.wi[row,7] <- SPE.wi.ald.2
+# } else if (j==3){
+#   analysis.wi[row,4] <- PPV.wi.ald.5
+#   analysis.wi[row,5] <- FDR.wi.ald.5
+#   analysis.wi[row,6] <- SEN.wi.ald.5
+#   analysis.wi[row,7] <- SPE.wi.ald.5
+# } else if (j==4){
+#   analysis.wi[row,4] <- PPV.wi.des
+#   analysis.wi[row,5] <- FDR.wi.des
+#   analysis.wi[row,6] <- SEN.wi.des
+#   analysis.wi[row,7] <- SPE.wi.des
+# } else if (j==5){
+#   analysis.wi[row,4] <- PPV.wi.des5
+#   analysis.wi[row,5] <- FDR.wi.des5
+#   analysis.wi[row,6] <- SEN.wi.des5
+#   analysis.wi[row,7] <- SPE.wi.des5
+# }
+# row=row+1
+# }
+# }
+# }
+# 
+# means.wi <- matrix(data=NA, nrow=30, ncol=6)
+# means.wi <- as.data.frame(means.wi)
+# colnames(means.wi) <- c("coef", "method", "PPV","FDR","SEN","SPE")
+# 
+# # don't do this!!!!!!
+# # ugly hack
+# for(i in 1:5){
+#   means.wi[i,1:2] <- analysis.wi[seq(from=i, to=50, by=5),c(1,3)][1,]
+#   means.wi[i,3:6] <- colMeans(analysis.wi[seq(from=i, to=50, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means.wi[i+5,1:2] <- analysis.wi[seq(from=i+50, to=100, by=5),c(1,3)][1,]
+#   means.wi[i+5,3:6] <- colMeans(analysis.wi[seq(from=i+50, to=100, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means.wi[i+10,1:2] <- analysis.wi[seq(from=i+100, to=150, by=5),c(1,3)][1,]
+#   means.wi[i+10,3:6] <- colMeans(analysis.wi[seq(from=i+100, to=150, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means.wi[i+15,1:2] <- analysis.wi[seq(from=i+150, to=200, by=5),c(1,3)][1,]
+#   means.wi[i+15,3:6] <- colMeans(analysis.wi[seq(from=i+150, to=200, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means.wi[i+20,1:2] <- analysis.wi[seq(from=i+201, to=250, by=5),c(1,3)][1,]
+#   means.wi[i+20,3:6] <- colMeans(analysis.wi[seq(from=i+201, to=250, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means.wi[i+25,1:2] <- analysis.wi[seq(from=i+251, to=300, by=5),c(1,3)][1,]
+#   means.wi[i+25,3:6] <- colMeans(analysis.wi[seq(from=i+251, to=300, by=5),4:7])
+# }
+# 
+# FN.ald <- setdiff(model, which(data.out[[i]]$ald0$we.eBH < 0.05))
+# FN.ald.5 <- setdiff(model, which(data.out[[i]]$ald5$we.eBH < 0.05))
+# FN.ald.2 <- setdiff(model, which(data.out[[i]]$ald2$we.eBH < 0.05))
+# FN.des <- setdiff(model, which(data.out[[i]]$des$padj < 0.05))
+# FN.des5 <- setdiff(model, which(data.out[[i]]$des$padj < 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5))
+# 
+# FP.ald <- intersect(which(data.out[[i]]$ald0$we.eBH < 0.05), null.model)
+# FP.ald.2 <- intersect(which(data.out[[i]]$ald2$we.eBH < 0.05), null.model)
+# FP.ald.5 <- intersect(which(data.out[[i]]$ald5$we.eBH < 0.05), null.model)
+# FP.des <- intersect(which(data.out[[i]]$des$padj < 0.05), null.model)
+# FP.des5 <- intersect(which(data.out[[i]]$des$padj < 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5), null.model)
+# 
+# 
+# TN.des <- intersect(which(data.out[[i]]$des$padj >= 0.05), null.model)
+# TN.des5 <- intersect(which(data.out[[i]]$des$padj >= 0.05 & abs(data.out[[i]]$des$log2FoldChange) > 0.5), null.model)
+# 
+# PPV.ald <- length(TP.ald)/sum(length(TP.ald),length(FP.ald))
+# PPV.ald.2 <- length(TP.ald.2)/sum(length(TP.ald.2),length(FP.ald.2))
+# PPV.ald.5 <- length(TP.ald.5)/sum(length(TP.ald.5),length(FP.ald.5))
+# PPV.des <- length(TP.des)/sum(length(TP.des),length(FP.des))
+# PPV.des5 <- length(TP.des5)/sum(length(TP.des5),length(FP.des5))
+# 
+# FDR.ald <- length(FP.ald)/sum(length(TP.ald),length(FP.ald))
+# FDR.ald.2 <- length(FP.ald.2)/sum(length(TP.ald.2),length(FP.ald.2))
+# FDR.ald.5 <- length(FP.ald.5)/sum(length(TP.ald.5),length(FP.ald.5))
+# FDR.des <- length(FP.des)/sum(length(TP.des),length(FP.des))
+# FDR.des5 <- length(FP.des5)/sum(length(TP.des5),length(FP.des5))
+# 
+# SEN.ald <- length(TP.ald)/(length(TP.ald) + length(FN.ald))
+# SEN.ald.2 <- length(TP.ald.2)/(length(TP.ald.2) + length(FN.ald.2))
+# SEN.ald.5 <- length(TP.ald.5)/(length(TP.ald.5) + length(FN.ald.5))
+# SEN.des <- length(TP.des)/(length(TP.des) + length(FN.des))
+# SEN.des5 <- length(TP.des5)/(length(TP.des5) + length(FN.des5))
+# 
+# SPE.ald <- length(TN.ald)/(length(TN.ald) + length(FP.ald))
+# SPE.ald.2 <- length(TN.ald.2)/(length(TN.ald.2) + length(FP.ald.2))
+# SPE.ald.5 <- length(TN.ald.5)/(length(TN.ald.5) + length(FP.ald.5))
+# SPE.des <- length(TN.des)/(length(TN.des) + length(FP.des))
+# SPE.des5 <- length(TN.des5)/(length(TN.des5) + length(FP.des5))
+# 
+# # even butt uglier ...
+# for (j in 1:5){
+#   analysis.out[row,1] <- coeff
+#   analysis.out[row,2] <- i
+# if (j==1){
+#   analysis.out[row,4] <- PPV.ald
+#   analysis.out[row,5] <- FDR.ald
+#   analysis.out[row,6] <- SEN.ald
+#   analysis.out[row,7] <- SPE.ald
+# } else if (j==2) {
+#   analysis.out[row,4] <- PPV.ald.2
+#   analysis.out[row,5] <- FDR.ald.2
+#   analysis.out[row,6] <- SEN.ald.2
+#   analysis.out[row,7] <- SPE.ald.2
+# } else if (j==3){
+#   analysis.out[row,4] <- PPV.ald.5
+#   analysis.out[row,5] <- FDR.ald.5
+#   analysis.out[row,6] <- SEN.ald.5
+#   analysis.out[row,7] <- SPE.ald.5
+# } else if (j==4){
+#   analysis.out[row,4] <- PPV.des
+#   analysis.out[row,5] <- FDR.des
+#   analysis.out[row,6] <- SEN.des
+#   analysis.out[row,7] <- SPE.des
+# } else if (j==5){
+#   analysis.out[row,4] <- PPV.des5
+#   analysis.out[row,5] <- FDR.des5
+#   analysis.out[row,6] <- SEN.des5
+#   analysis.out[row,7] <- SPE.des5
+# }
+# row=row+1
+# }
+# }
+# }
+# 
+# means <- matrix(data=NA, nrow=30, ncol=6)
+# means <- as.data.frame(means)
+# colnames(means) <- c("coef", "method", "PPV","FDR","SEN","SPE")
+# 
+# for(i in 1:5){
+#   means[i,1:2] <- analysis.out[seq(from=i, to=50, by=5),c(1,3)][1,]
+#   means[i,3:6] <- colMeans(analysis.out[seq(from=i, to=50, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means[i+5,1:2] <- analysis.out[seq(from=i+50, to=100, by=5),c(1,3)][1,]
+#   means[i+5,3:6] <- colMeans(analysis.out[seq(from=i+50, to=100, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means[i+10,1:2] <- analysis.out[seq(from=i+100, to=150, by=5),c(1,3)][1,]
+#   means[i+10,3:6] <- colMeans(analysis.out[seq(from=i+100, to=150, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means[i+15,1:2] <- analysis.out[seq(from=i+150, to=200, by=5),c(1,3)][1,]
+#   means[i+15,3:6] <- colMeans(analysis.out[seq(from=i+150, to=200, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means[i+20,1:2] <- analysis.out[seq(from=i+201, to=250, by=5),c(1,3)][1,]
+#   means[i+20,3:6] <- colMeans(analysis.out[seq(from=i+201, to=250, by=5),4:7])
+# }
+# for(i in 1:5){
+#   means[i+25,1:2] <- analysis.out[seq(from=i+251, to=300, by=5),c(1,3)][1,]
+#   means[i+25,3:6] <- colMeans(analysis.out[seq(from=i+251, to=300, by=5),4:7])
+# }
+# 
+# 
+# ald.row <- which(means$method == "ald")
+# ald2.row <- which(means$method == "ald2")
+# ald5.row <- which(means$method == "ald5")
+# des.row <- which(means$method == "des")
+# des5.row <- which(means$method == "des5")
