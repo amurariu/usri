3eae04c21bf88ef38095cdf379e64320158c7196
amurariu <amurariu@uwo.ca>
2025-06-16 14:09:45 -0400

    Update limma.R

--- code/limma.R
+++ code/limma.R
@@ -16,7 +16,7 @@ m <- read.table(file=meta, header=F, row.names=1, sep='\t')
 #establishing conditions for PD1
 conditions_p <- rep("Pre", 109) #use str to figure out what conds p is
 conditions_p[grep("_On",m)] <- "On"
-immuno.conds<- data.frame(conditions_p)
+immuno.conds<- as.vector(conditions_p)
 
 
 #edgeR conditions for initial filtering
@@ -35,7 +35,7 @@ con <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-BRCA.condi
 
 brca <- read.table(file=raw_counts, header=T, row.names=1, sep='\t')
 conditions_b <- as.vector(unlist(read.table(file=con, sep='\t'))) #changed from brca.conds to conditions_b
-brca.conds <- data.frame(conditions_b) #changed from conditions to brca.conds for consistency with PD1 dataset
+brca.conds <- as.vector(conditions_b) #changed from conditions to brca.conds for consistency with PD1 dataset
 
 #edgeR
 y_brca <- DGEList(counts=brca, group=factor(conditions_b))
@@ -46,8 +46,8 @@ brca.data <- y_brca$counts #filtered base dataset
 # immuno is the data table
 # immuno.conds is the conditions for the unpermuted data
 # N is the number of random instances
-immuno.data.limma <- lim.fun(immuno.data, immuno.conds, 1)
+immuno.data.limma <- lim.fun(immuno.data, immuno.conds, 100)
 save(immuno.data.limma, file="../ext_analysis/immuno.data.limma.Rda") 
 
-brca.data.limma <- lim.fun(brca.data, brca.conds, 1)
+brca.data.limma <- lim.fun(brca.data, brca.conds, 100)
 save(brca.data.limma, file="../ext_analysis/brca.data.limma.Rda") 
