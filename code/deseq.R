48420471c1f34487d721dc5331f8b2e77c68ed03
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-06-16 16:58:13 -0400

    bugfix for deseq2 error (plus formatting)
    - Fixed warning about formatting design formula as factor for DESeq2. Previously, command was treating 1s and 0s as a numeric to "specify a model with increasing fold change for higher value". Corrected so 1 and 0 represent 2 different groups.
    
    - Added more verbose comments to 'des.fun.R'
    
    - Automated formula argument in deseq2 calls
    
    - Renamed variables across function to distinguish randomised vs. thinned data (previously r vs. p; now is r vs. t)
    
    - Added set.seed() call prior to non-randomised, non-thinned analysis so it will always be reproducible
    
    - Non-randomised, non-thinned analysis now output to list initialised at start (previously was stored in new object, making 'data.out.deseq.u' redundant)

--- code/deseq.R
+++ code/deseq.R
@@ -1,25 +1,33 @@
+# code for generating DESeq2 data x100 (all datasets)
+
+# Author: Andreea Murariu
+# Last edited: 2025-06-16
+
 library(seqgendiff, warn.conflicts=F)
 library(edgeR, warn.conflicts=F)
 library(DESeq2, warn.conflicts=F)
 
-# loads the DESeq2 function to permute and add random true positives to the dataset
+# loads the DESeq2 function to randomise groups and add random true positives to
+# the count tables
 source('code/des.fun.R')
 
 #####
 # PD1 immunotherapy dataset
 ####
 
-# pull the data and filter using edgeR
+# pull the raw count data from github
 raw_counts <- 'https://raw.githubusercontent.com/amurariu/usri/main/data/imm-GSE91061_raw_counts_GRCh38.p13_NCBI.tsv'
 meta <- 'https://raw.githubusercontent.com/amurariu/usri/main/data/imm_metadata.txt'
-immuno<-read.table(file=raw_counts, header = T, skip=35, sep='\t', row.names = 1)
+
+immuno <- read.table(file=raw_counts, header = T, skip=35, sep='\t', row.names = 1)
 m <- read.table(file=meta, header=F, row.names=1, sep='\t')
-#establishing conditions for PD1
+
+# make conditions vector for PD1 dataset using metadata info (Pre vs. On)
 conditions_p <- rep("Pre", 109)
 conditions_p[grep("_On",m)] <- "On"
 immuno.conds <- data.frame(conditions_p)
 
-#edgeR conditions for initial filtering
+# edgeR conditions for initial filtering
 y_pd1 <- DGEList(counts=immuno, group=factor(conditions_p))
 keep_pd1 <- filterByExpr(y_pd1)
 y_pd1 <- y_pd1[keep_pd1,keep.lib.sizes=FALSE]
@@ -29,7 +37,7 @@ immuno.data <- y_pd1$counts #filtered base dataset
 #BRCA dataset
 #####
 
-#pull the data and filter
+#pull the data and filter as above
 raw_counts<- "https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-BRCA.normal-tumor.pair.rawCount.tsv"
 con <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-BRCA.conditions.tsv"
 
@@ -37,16 +45,16 @@ brca <- read.table(file=raw_counts, header=T, row.names=1, sep='\t')
 conditions_b <- as.vector(unlist(read.table(file=con, sep='\t'))) #changed from brca.conds to conditions_b
 brca.conds <- data.frame(conditions_b) #changed from conditions to brca.conds for consistency with PD1 dataset
 
-#edgeR
 y_brca <- DGEList(counts=brca, group=factor(conditions_b))
 keep_brca <- filterByExpr(y_brca)
 y_brca <- y_brca[keep_brca,keep.lib.sizes=FALSE]
 brca.data <- y_brca$counts #filtered base dataset
 
 # immuno is the data table
-# immuno.conds is the conditions for the unpermuted data
-# N is the number of random instances
-immuno.data.DESeq <- des.fun(immuno.data, immuno.conds, 100)
+# immuno.conds is the vector of conditions for the non-randomised data
+# nloop is the number of randomisation instances for randomised data
+immuno.data.DESeq <- des.fun(data = immuno.data, nloop = 100, conditions = immuno.conds$conditions_p)
+
 save(immuno.data.DESeq, file="../ext_analysis/immuno.data.deseq.Rda") 
 
 #BRCA function
