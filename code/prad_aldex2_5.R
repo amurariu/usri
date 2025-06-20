0fa9354d59f777bc2f6774debe78e4d786ab4057
amurariu <amurariu@uwo.ca>
2025-06-20 13:26:40 -0400

    new run files for aldex separated by scale

---
+++ code/prad_aldex2_5.R
@@ -0,0 +1,24 @@
+library(ALDEx2,warn.conflicts = F) #do not load aldex2 and aldex3 at the same time
+library(seqgendiff, warn.conflicts=F)
+library(edgeR, warn.conflicts=F)
+
+source('code/ald2.fun.R')
+
+#####
+#PRAD dataset
+#####
+
+raw_counts_prad <- "https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-PRAD.normal-tumor.pair.rawCount.tsv"
+conds_prad <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-PRAD.conditions.tsv"
+prad <- read.table(file=raw_counts_prad, header=T, row.names=1, sep='\t')
+conditions_pr <- as.vector(unlist(read.table(file=conds_prad, sep='\t')))
+prad.conds <- data.frame(conditions_pr) 
+
+y_prad <- DGEList(counts=prad, group=factor(conditions_pr))
+keep_prad <- filterByExpr(y_prad)
+y_prad <- y_prad[keep_prad,keep.lib.sizes=FALSE]
+prad.data <- y_prad$counts #filtered base dataset
+
+#save file - gamma=0.5
+prad.data_5.aldex2 <- ald2.fun(data=prad.data, conditions=prad.conds, nloop=100, gamma = 0.5)
+save(prad.data_5.aldex2, file="../ext_analysis/prad.data.aldex2_5.out.Rda")
---
+++ code/thca_aldex2_5.R
@@ -0,0 +1,24 @@
+library(ALDEx2,warn.conflicts = F) #do not load aldex2 and aldex3 at the same time
+library(seqgendiff, warn.conflicts=F)
+library(edgeR, warn.conflicts=F)
+
+source('code/ald2.fun.R')
+
+#####
+#THCA dataset
+#####
+
+raw_counts_thca <- "https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-THCA.normal-tumor.pair.rawCount.tsv"
+conds_thca <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-THCA.conditions.tsv"
+thca <- read.table(file=raw_counts_thca, header=T, row.names=1, sep='\t')
+conditions_t <- as.vector(unlist(read.table(file=conds_thca, sep='\t')))
+thca.conds <- data.frame(conditions_t) 
+
+y_thca <- DGEList(counts=thca, group=factor(conditions_t))
+keep_thca <- filterByExpr(y_thca)
+y_thca <- y_thca[keep_thca,keep.lib.sizes=FALSE]
+thca.data <- y_thca$counts #filtered base dataset
+
+#save file - gamma=0.5
+thca.data_5.aldex2 <- ald2.fun(data=thca.data, conditions=thca.conds, nloop=100, gamma = 0.5)
+save(thca.data_5.aldex2, file="../ext_analysis/thca.data.aldex2_5.out.Rda")
