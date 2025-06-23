2c984adf272b4599a74223303e85826db6d5b3b7
amurariu <amurariu@uwo.ca>
2025-06-23 09:24:22 -0400

    brca

--- code/brca_deseq.R
+++ code/brca_deseq.R
@@ -12,8 +12,8 @@ source('code/des.fun.R')
 raw_counts_brca<- "https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-BRCA.normal-tumor.pair.rawCount.tsv"
 conds_brca <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-BRCA.conditions.tsv"
 
-brca <- read.table(file=raw_counts, header=T, row.names=1, sep='\t')
-conditions_b <- as.vector(unlist(read.table(file=con, sep='\t'))) #changed from brca.conds to conditions_b
+brca <- read.table(file=raw_counts_brca, header=T, row.names=1, sep='\t')
+conditions_b <- as.vector(unlist(read.table(file=conds_brca, sep='\t'))) #changed from brca.conds to conditions_b
 brca.conds <- data.frame(conditions_b) 
 
 y_brca <- DGEList(counts=brca, group=factor(conditions_b))
