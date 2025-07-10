2690d6f2f75e71a35ffbe99589c59ab74c2623d2
amurariu <amurariu@uwo.ca>
2025-07-10 15:18:07 -0400

    cdiff

--- code/humanhiv_deseq.R
+++ code/humanhiv_deseq.R
@@ -8,8 +8,8 @@ source('code/des.fun.R')
 #Human HIV (3) dataset
 #####
 
-raw_counts_hiv <- "https://raw.githubusercontent.com/amurariu/usri/main/data/name.tsv"
-conds_hiv <-"https://raw.githubusercontent.com/amurariu/usri/main/data/name.tsv"
+raw_counts_hiv <- "https://raw.githubusercontent.com/amurariu/usri/main/data/hiv_noguerajulian_ASVs_table.tsv"
+conds_hiv <-"https://raw.githubusercontent.com/amurariu/usri/main/data/hiv_noguerajulian_metadata.tsv"
 hiv <- read.table(file=raw_counts_hiv, header=T, row.names=1, sep='\t')
 conditions_h <- as.vector(unlist(read.table(file=conds_hiv, sep='\t')))
 hiv.conds <- data.frame(conditions_h) 
