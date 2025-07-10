bd66c429498ddaf680d35ad7c286a9f04af88fb8
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-07-10 11:02:35 -0400

    ALDEx2 & DESeq2 for mts data
    - Added code for running aldex2 (0/0.2/0.5 gamma) and deseq2 on vaginal metatranscriptome data
    
    - Added examples showing that adding pseudocounts of 1 to all features is a common practice to avoid a specific DESeq2 error ('every gene contains at least one zero, cannot compute log geometric means')

---
+++ code/mts_aldex2_0.R
@@ -0,0 +1,27 @@
+# running aldex2 (0 gamma) on vaginal metatranscriptome dataset (healthy vs. BV)
+
+# Scott Dos Santos
+# 2025-07-10
+
+library(ALDEx2) #do not load aldex2 and aldex3 at the same time
+library(seqgendiff)
+
+# load ALDEx2 function for generating randomised/thinned data
+source('code/ald2.fun.R')
+
+# load metatranscriptome (mts) dataset and metadata
+loc.counts <- "https://github.com/amurariu/usri/raw/refs/heads/main/data/mts.data.filt.txt"
+loc.meta <- "https://github.com/amurariu/usri/raw/refs/heads/main/data/mts.metadata.txt"
+
+mts.counts <- read.table(loc.counts, header = T, sep = "\t", quote = "", row.names = 1)
+mts.meta <- read.table(loc.meta, header = T, sep = "\t", quote = "", row.names = 1)
+
+# convert count table to matrix
+mts.counts <- as.matrix(mts.counts)
+
+# generate results for thinned +/- randomised mts data, 100 times, plus 1x
+# non-randomised, non-thinned (i.e. original data)
+mts.data.aldex2.0 <- ald2.fun(data = mts.counts, nloop = 100,
+                              conditions = mts.meta$groups.2, gamma = 1e-3)
+
+save(mts.data.aldex2.0, file = "../ext_analysis/mts.data.aldex2_0.Rda")
