2347b7437267fb7f3e3e2bd7dbcfdede1cf6e2c2
amurariu <amurariu@uwo.ca>
2025-07-10 10:22:32 -0400

    Update kirc_aldex3_2.R

--- code/kirc_aldex3_2.R
+++ code/kirc_aldex3_2.R
@@ -20,5 +20,5 @@ keep_kirc <- filterByExpr(y_kirc)
 y_kirc <- y_kirc[keep_kirc,keep.lib.sizes=FALSE]
 kirc.data <- y_kirc$counts #filtered base dataset
 
-kirc.data_2.aldex3 <- ald3.fun(data=kirc.data, conds=kirc.conds$conditions_k, nloop=1, gamma=0.2)
+kirc.data_2.aldex3 <- ald3.fun(data=kirc.data, conds=kirc.conds$conditions_k, nloop=100, gamma=0.2)
 save(kirc.data_2.aldex3, file="/Volumes/data2/andreea/ext_analysis/kirc.data.aldex3_2.Rda")
