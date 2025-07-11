d99e4eaa53514785655bfea7c6f0a3b14539cfc0
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-07-11 15:26:20 -0400

    cpn60 ALDEx2/ALDEx3/edgeR
    - Added code for running ALDEx2/ALDEx3 at gamma of (virtually) 0, 0.1, 0.2, and 0.5 on cpn60 infant stool dataset (perinatal abx exposure vs. no exposure)

---
+++ code/cpn60_edgeR.R
@@ -0,0 +1,35 @@
+# running edgeR on cpn60 dataset (ST3, abx vs. no abx exposure)
+
+# Scott Dos Santos
+# 2025-07-11
+
+library(seqgendiff)
+library(edgeR)
+
+# load edgeR function for generating randomised/thinned data
+source('code/edg.fun.R')
+
+# load cpn60 dataset and metadata
+loc.counts <- "https://github.com/amurariu/usri/raw/refs/heads/main/data/cpn60.data.txt"
+loc.meta <- "https://github.com/amurariu/usri/raw/refs/heads/main/data/cpn60.metadata.txt"
+
+cpn60.counts <- read.table(loc.counts, header = T, sep = "\t", quote = "", row.names = 1)
+cpn60.meta <- read.table(loc.meta, header = T, sep = "\t", quote = "", row.names = 1)
+
+# substitute periods for dashes in column names of table
+colnames(cpn60.counts) <- gsub("\\.", "-", colnames(cpn60.counts))
+
+# filter for only ST3 samples (10 day-old infant stool)
+cpn60.meta <- cpn60.meta[which(cpn60.meta$age == "10 days"),]
+cpn60.counts <- cpn60.counts[, rownames(cpn60.meta)]
+
+# check for and remove ASVs with zero sums
+any(rowSums(cpn60.counts) == 0) # returns TRUE
+cpn60.counts <- cpn60.counts[-which(rowSums(cpn60.counts)==0),]
+
+# convert count table to matrix
+cpn60.counts <- as.matrix(cpn60.counts)
+
+# generate results for thinned +/- randomised cpn60 data, 100 times, plus 1x
+# non-randomised, non-thinned (i.e. original data)
+cpn60.data.edgeR <- edg.fun(data = cpn60.counts, conditions = cpn60.meta$abx, nloop = 100)
