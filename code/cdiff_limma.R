d254db8eb2e82a52207c549b168c50d52c794636
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-07-11 12:02:07 -0400

    limma input fix

--- code/cdiff_limma.R
+++ code/cdiff_limma.R
@@ -9,11 +9,11 @@ source('code/lim.fun.R')
 
 raw_counts_cdiff <- "https://raw.githubusercontent.com/amurariu/usri/main/data/cdi_schubert_ASVs_table.tsv"
 conds_cdiff <-"https://raw.githubusercontent.com/amurariu/usri/main/data/cdi_schubert_metadata.tsv"
+
 cdiff <- read.table(file=raw_counts_cdiff, header=T, row.names=1, sep='\t')
 conditions_c <- read.table(file=conds_cdiff, sep='\t', row.names = 1, header = T)
-cdiff.conds <- as.vector(unlist(conditions_c))
-transposed_cdiff<- t(cdiff)
 
-cdiff.data.limma <- lim.fun(data = transposed_cdiff, conditions = cdiff.conds, nloop = 100, mean = 0, prop_null = 0.95)
+
+cdiff.data.limma <- lim.fun(data = as.matrix(cdiff), conditions = conditions_c$comparison, nloop = 2, mean = 0, prop_null = 0.95)
 save(cdiff.data.limma, file="/Volumes/data2/andreea/ext_analysis/cdiff.data.limma.Rda") 
 
