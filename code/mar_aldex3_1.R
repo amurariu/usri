f6a9b397a56f10fb65cc8557e036850a69fa19df
amurariu <amurariu@uwo.ca>
2025-07-16 13:15:45 -0400

    as.matrix fix for aldex3

--- code/mar_aldex3_1.R
+++ code/mar_aldex3_1.R
@@ -16,5 +16,5 @@ mar <- mar[,-ncol(mar)]
 conditions_m <- as.vector(unlist(read.table(file=conds_mar, sep='\t')))
 mar.conds <- data.frame(conditions_m) 
 
-mar.data_1.aldex3 <- ald3.fun(data=mar, conds=mar.conds$comparison, nloop=100, gamma=0.1)
+mar.data_1.aldex3 <- ald3.fun(data=as.matrix(mar), conds=mar.conds$comparison, nloop=100, gamma=0.1)
 save(mar.data_1.aldex3, file="/Volumes/data2/andreea/ext_analysis/mar.data.aldex3_1.Rda")
