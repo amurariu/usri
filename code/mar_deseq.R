3a6d8b26f40f292923a4c7241b9705d7e4414460
amurariu <amurariu@uwo.ca>
2025-07-18 12:30:15 -0400

    Update mar_deseq.R

--- code/mar_deseq.R
+++ code/mar_deseq.R
@@ -11,13 +11,12 @@ source('code/des.fun.R')
 raw_counts_mar <- "https://raw.githubusercontent.com/amurariu/usri/main/data/sw_sed_detender_ASVs_table.tsv"
 conds_mar <-"https://raw.githubusercontent.com/amurariu/usri/main/data/sw_sed_detender_metadata.tsv"
 mar <- read.table(file=raw_counts_hiv, header=T, row.names=1, sep='\t')
-mar <- as.matrix(mar)
-mar <- mar+1
-conditions_m <- read.table(file=conds_mar, sep='\t', row.names = 1, header = T)
-mar.conds <- data.frame(conditions_m) 
+mar <- read.table(file=raw_counts_mar, header=T, row.names=1, sep='\t')
+taxa <- mar$taxonomy
+mar$taxonomy <- NULL
+conditions_m <- read.table(file=conds_mar, sep='\t', header = T)
 transposed_mar<- t(mar)
 
-
 # function
-mar.data.DESeq <- des.fun(data = transposed_mar, nloop = 100, conditions = mar.conds$comparison)
+mar.data.DESeq <- des.fun(data = mar, nloop = 100, conditions = mar.conds$comparison)
 save(mar.data.DESeq, file="/Volumes/data2/andreea/ext_analysis/mar.data.deseq.Rda") 
