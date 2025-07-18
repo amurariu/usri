47b325a4f1309f7992d02d68daa1722b64517554
amurariu <amurariu@uwo.ca>
2025-07-18 12:34:16 -0400

    Update mar_deseq.R

--- code/mar_deseq.R
+++ code/mar_deseq.R
@@ -11,6 +11,7 @@ source('code/des.fun.R')
 raw_counts_mar <- "https://raw.githubusercontent.com/amurariu/usri/main/data/sw_sed_detender_ASVs_table.tsv"
 conds_mar <-"https://raw.githubusercontent.com/amurariu/usri/main/data/sw_sed_detender_metadata.tsv"
 mar <- read.table(file=raw_counts_mar, header=T, row.names=1, sep='\t')
+mar <- mar+1
 taxa <- mar$taxonomy
 mar$taxonomy <- NULL
 conditions_m <- read.table(file=conds_mar, sep='\t', header = T)
