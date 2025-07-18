2d3c85dbe2ba13dac85524ffcbefdbbddab57c6a
amurariu <amurariu@uwo.ca>
2025-07-18 12:48:59 -0400

    Update mar_deseq.R

--- code/mar_deseq.R
+++ code/mar_deseq.R
@@ -20,5 +20,5 @@ conditions_m <- read.table(file=conds_mar, row.names = 1, sep='\t', header = T)
 transposed_mar<- t(mar)
 
 # function
-mar.data.DESeq <- des.fun(data = as.matrix(mar), nloop = 100, conditions = mar.conds$comparison)
+mar.data.DESeq <- des.fun(data = mar, nloop = 100, conditions = mar.conds$comparison)
 save(mar.data.DESeq, file="/Volumes/data2/andreea/ext_analysis/mar.data.deseq.Rda") 
