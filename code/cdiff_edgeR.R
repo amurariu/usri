dc8d338a87bfdf46d03603fb92477b4a3f69817a
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-07-28 10:26:55 -0400

    fix filtering for 16S dataset
    - Scott was dumb and implemented filter to keep only ASVs present in >90 % of samples, but it SHOULD have been keep all ASVs present in >10 %, like in original paper. This corrects the filter.

--- code/cdiff_edgeR.R
+++ code/cdiff_edgeR.R
@@ -13,6 +13,9 @@ cdiff <- read.table(file=raw_counts_cdiff, header=T, row.names=1, sep='\t')
 conditions_c <- read.table(file=conds_cdiff, sep='\t', row.names = 1, header = T)
 cdiff.conds <- data.frame(conditions_c) 
 
+# filter to remove any ASV present in <10 % of samples
+cdiff.filt <- cdiff[apply(cdiff, 1, function(x){length(which(x != 0))/length(x)} >0.1),]
+
 # function
 cdiff.data.edgeR <- edg.fun(data = as.matrix(cdiff), conditions = cdiff.conds$comparison, nloop = 100)
 save(cdiff.data.edgeR, file="/Volumes/data2/andreea/ext_analysis/cdiff.data.edgeR.Rda") 
