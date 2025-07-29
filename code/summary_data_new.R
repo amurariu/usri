5902f914ac50abcfbf2d3fa6128f830dc088b783
amurariu <amurariu@uwo.ca>
2025-07-29 11:35:51 -0400

    summary_data graph changes
    graph for minimum absolute difference needed for significance

--- code/summary_data_new.R
+++ code/summary_data_new.R
@@ -1,3 +1,8 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
 source('code/summary.data.fun.R')
 
 load(paste(anal.path,"immuno.data.aldex2_0.Rda", sep=""))
@@ -9,7 +14,82 @@ load(paste(anal.path,"immuno.data.aldex3_1.Rda", sep=""))
 load(paste(anal.path,"immuno.data.aldex3_2.Rda", sep=""))
 load(paste(anal.path,"immuno.data.aldex3_5.Rda", sep=""))
 
-sum.fun(aldex2_0 = immuno.data_0.aldex2, aldex2_1 = immuno.data_1.aldex2, aldex2_2 = immuno.data_2.aldex2, aldex2_5 = immuno.data_5.aldex2, aldex3_0 = immuno.data_0.aldex3, aldex3_1 = immuno.data_1.aldex3, aldex3_2 = immuno.data_2.aldex3, aldex3_5 = immuno.data_5.aldex3)
+imm.sum <- sum.fun(aldex2_0 = immuno.data_0.aldex2, aldex2_1 = immuno.data_1.aldex2, aldex2_2 = immuno.data_2.aldex2, aldex2_5 = immuno.data_5.aldex2, aldex3_0 = immuno.data_0.aldex3, aldex3_1 = immuno.data_1.aldex3, aldex3_2 = immuno.data_2.aldex3, aldex3_5 = immuno.data_5.aldex3)
+
+p1<-imm.sum$minmax2_0 %>% 
+  ggplot(aes(x = abs)) +
+  geom_density(colour='darkblue', fill='darkslategray1', alpha=0.25) + 
+  geom_vline(aes(xintercept=mean(abs)),
+             color="blue", linetype="dashed", linewidth=0.5) +
+  theme(panel.background = element_rect(fill = "gray95"), axis.line=element_line(colour="black", linewidth=0.5))
+
+p2<-imm.sum$minmax2_1 %>% 
+  ggplot(aes(x = abs)) +
+  geom_density(colour='darkblue', fill='darkslategray1', alpha=0.25) + 
+  geom_vline(aes(xintercept=mean(abs)),
+             color="blue", linetype="dashed", linewidth=0.5) +
+  theme(panel.background = element_rect(fill = "gray95"), axis.line=element_line(colour="black", linewidth=0.5))
+
+p3<-imm.sum$minmax2_2 %>% 
+  ggplot(aes(x = abs)) +
+  geom_density(colour='darkblue', fill='darkslategray1', alpha=0.25) + 
+  geom_vline(aes(xintercept=mean(abs)),
+             color="blue", linetype="dashed", linewidth=0.5) +
+  theme(panel.background = element_rect(fill = "gray95"), axis.line=element_line(colour="black", linewidth=0.5))
+
+
+#plot combining all values for aldex2 and aldex3 - starts here -----------------------------------------
+df1 <- as.data.frame(imm.sum$minmax2_0)
+df2 <- as.data.frame(imm.sum$minmax2_1)
+df3 <- as.data.frame(imm.sum$minmax2_2)
+df4 <- as.data.frame(imm.sum$minmax2_5)
+df1$group<-'scale=0'
+df2$group<-'scale=0.1'
+df3$group<-'scale=0.2'
+df4$group<-'scale=0.5'
+df_merged1 = bind_rows(df1,df2,df3,df4)
+
+group_means1 <- df_merged1 %>%
+  group_by(group) %>%
+  summarise(mean_value1 = mean(abs))
+
+p1<-df_merged1 %>% 
+  ggplot(aes(x = abs, fill=group)) + 
+  geom_density(alpha=0.5)+
+  geom_vline(data = group_means1, aes(xintercept = mean_value1, colour=group), linetype="dashed", linewidth=0.5) +
+  theme(panel.background = element_rect(fill = "gray95"), axis.line=element_line(colour="black", linewidth=0.5))+
+  xlim(0,2)+
+  ylim(0,10)+
+  labs(title = "ALDEx2 Minimum Absolute Difference")
+
+
+df5 <- as.data.frame(imm.sum$minmax3_0)
+df6 <- as.data.frame(imm.sum$minmax3_1)
+df7 <- as.data.frame(imm.sum$minmax3_2)
+df8 <- as.data.frame(imm.sum$minmax3_5)
+df5$group<-'scale=0'
+df6$group<-'scale=0.1'
+df7$group<-'scale=0.2'
+df8$group<-'scale=0.5'
+df_merged2 = bind_rows(df5,df6,df7,df8)
+
+group_means2 <- df_merged2 %>%
+  group_by(group) %>%
+  summarise(mean_value2 = mean(abs))
+
+p2<-df_merged2 %>% 
+  ggplot(aes(x = abs, fill=group)) + 
+  geom_density(alpha=0.5)+
+  geom_vline(data = group_means2, aes(xintercept = mean_value2, colour=group), linetype="dashed", linewidth=0.5) +
+  theme(panel.background = element_rect(fill = "gray95"), axis.line=element_line(colour="black", linewidth=0.5))+
+  xlim(0,2)+
+  ylim(0,10)+
+  labs(title = "ALDEx3 Minimum Absolute Difference")
+
+p1|p2
+
+#2 panel figure code ends here---------------------------------------------------------
+
 
 load(paste(anal.path,"lihc.data.aldex2_0.Rda", sep=""))
 load(paste(anal.path,"lihc.data.aldex2_1.Rda", sep=""))
@@ -20,4 +100,8 @@ load(paste(anal.path,"lihc.data.aldex3_1.Rda", sep=""))
 load(paste(anal.path,"lihc.data.aldex3_2.Rda", sep=""))
 load(paste(anal.path,"lihc.data.aldex3_5.Rda", sep=""))
 
-sum.fun(aldex2_0 = lihc.data_0.aldex2, aldex2_1 = lihc.data_1.aldex2, aldex2_2 = lihc.data_2.aldex2, aldex2_5 = lihc.data_5.aldex2, aldex3_0 = lihc.data_0.aldex3, aldex3_1 = lihc.data_1.aldex3, aldex3_2 = lihc.data_2.aldex3, aldex3_5 = lihc.data_5.aldex3)
+lihc.sum <- sum.fun(aldex2_0 = lihc.data_0.aldex2, aldex2_1 = lihc.data_1.aldex2, aldex2_2 = lihc.data_2.aldex2, aldex2_5 = lihc.data_5.aldex2, aldex3_0 = lihc.data_0.aldex3, aldex3_1 = lihc.data_1.aldex3, aldex3_2 = lihc.data_2.aldex3, aldex3_5 = lihc.data_5.aldex3)
+
+
+
+
