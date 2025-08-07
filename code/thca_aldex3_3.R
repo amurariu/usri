cbbb89517efaf6351176659cb6289311a22e4919
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-08-07 11:04:49 -0400

    add 0.3 & 0.4 gamma for A2/A3
    - Added scripts for all transcriptome/singlecell/metatranscriptome datasets to run ALDEx2 and ALDEx3 at gamma values of 0.3 and 0.4

---
+++ code/thca_aldex3_3.R
@@ -0,0 +1,24 @@
+devtools::load_all('~/Documents/ALDEx3')
+library(seqgendiff, warn.conflicts=F)
+library(edgeR, warn.conflicts=F)
+
+source('code/ald3.fun.R')
+
+#####
+#THCA dataset
+#####
+
+raw_counts_thca <- "https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-THCA.normal-tumor.pair.rawCount.tsv"
+conds_thca <-"https://raw.githubusercontent.com/amurariu/usri/main/data/TCGA-THCA.conditions.tsv"
+thca <- read.table(file=raw_counts_thca, header=T, row.names=1, sep='\t')
+conditions_t <- as.vector(unlist(read.table(file=conds_thca, sep='\t')))
+thca.conds <- data.frame(conditions_t) 
+
+y_thca <- DGEList(counts=thca, group=factor(conditions_t))
+keep_thca <- filterByExpr(y_thca)
+y_thca <- y_thca[keep_thca,keep.lib.sizes=FALSE]
+thca.data <- y_thca$counts #filtered base dataset
+
+#save file - gamma = 0.3
+thca.data_3.aldex3 <- ald3.fun(data=thca.data, conds=thca.conds$conditions_t, nloop=100, gamma=0.3)
+save(thca.data_3.aldex3, file="/Volumes/data2/andreea/ext_analysis/thca.data.aldex3_3.Rda")
