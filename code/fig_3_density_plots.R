61fb7905c76d239f604fe7fbc4c00c317f7d0b4a
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2025-08-18 12:51:05 -0400

    finalise density plots & stripchart code
    - Merged code from AM and SDS to finalise plots showing: 1) density plots of minimum diff.btw needed for significant result across 100 iterations (immuno data as example) for ALDEx2 and ALDEx3
    
    - Added figures for density plots & stripcharts (coloured by dataset or by tool) separately, as well as side-by-side version

--- code/fig_3_density_plots.R
+++ code/fig_3_density_plots.R
@@ -1,34 +1,52 @@
-library(tidyr)
+# density plots and strip chart for example dataset: ALDEx2 vs. ALDEx3
+
+# Andreea Murariu & Scott Dos Santos
+# Last edited: 2025-08-15
+
+library(stringr)
 library(dplyr)
+library(RColorBrewer)
 library(ggplot2)
 library(patchwork)
-library(stringr)
-library(ALDEx2,warn.conflicts = F) 
 
-
-anal.path <- "../ext_analysis/"
+# set paths to directory containing all analysis objects/repo
 data <- "~/Documents/GitHub/ext_analysis/"
 repo <- "~/Documents/GitHub/usri/"
 
+############################ summarise min diff.btw ############################
+
+# source function for extracting relevant data
 source('code/summary.data.fun.R')
 
-if(length(list.files(paste0(repo,"analysis/summarystats"), pattern = "ss")) != 8){
+# load in analysis data and get summary objects for all objects (or load them 
+# if they already exist as .Rda files) 
+
+# NOTE: Same deal for this section as in figure 1 sensitivity/fdr plots: I had 
+#       to run the for loops below individually to create the 'ss.' objects 
+#       for each dataset to avoid memory issues on local machine. Code is set
+#       up to load all 'ss.' objects in if they all exist in the corresponding
+#       directory.
+
+if(length(list.files(paste0(repo,"analysis/summarystats"), pattern = "ss")) != 10){
   
   # load in analysis results (takes a few minutes!)
-  for(i in list.files(data, pattern = "immuno.*aldex")){
+  for(i in list.files(data, pattern = "sccyto.*aldex")){
     load(paste0(data, i))
   }
   
-  # build summary object from datasets (will take several minutes)
-  i <- ls(pattern = "immuno")[1]
-    
-  # extract dataset and tool names from input file (sensitive to aldex gamma)
+  # extract dataset and tool names from input file
+  i <- ls(pattern = "sccyto")[1]
+  
   dataset <- str_split(i, "\\." , 3)[[1]][1]
   gamma <- "data_"
   tool <- "aldex"
     
-  # run 'summary statistics' on analysis objects to automatically build the
-  # confusion matrices
+  # run 'summary statistics' on analysis objects to extract the (absolute)
+  # minimum difference between group values that is needed for a significant
+  # BH-corrected P value at different gamma values (absolute as diff.btw
+  # will be positive or negative depending on the direction of the difference).
+  # max = positive values, min = negative values, abs = absolute values of
+  # min column followed by absolute values of max column.
   assign(x = paste0("ss.", dataset),
          sum.fun(aldex2_0 = get(paste0(dataset, ".", gamma, 0, ".", tool, 2)),
                  aldex2_1 = get(paste0(dataset, ".", gamma, 1, ".", tool, 2)),
@@ -44,21 +62,24 @@ if(length(list.files(paste0(repo,"analysis/summarystats"), pattern = "ss")) != 8
                  aldex3_5 = get(paste0(dataset, ".", gamma, 5, ".", tool, 3))))
   
   
-  # save confusion matrix list objects as .Rda
+  # save summary list objects as .Rda
   # NOTE: for MTS dataset, iteration 73 of ALDEx2 at gamma = 0.5 had NO
   # significant P values for the negative diff.btw features and so returns
-  # -Inf for the 73rd value in 'max' column and 173rd value in 'abs' column
+  # -Inf for the 73rd value in 'max' column and 173rd value in 'abs' column.
+  # Same issue present in single cell dataset: 28 Inf values (3 in ALDEx2 - 0.3,
+  # 7 in ALDEx2 - 0.4, 13 in ALDEx2 - 0.5, 1 in ALDEx3 - 0.4, 4 in ALDEx3 - 0.5)
     save(list = paste0("ss.", dataset),
          file = paste0(repo,"analysis/summarystats/ss.", dataset, ".Rda"))
   
   # finally, remove analysis objects from environment
-  for(i in ls(pattern = "^immuno")){
+  for(i in ls(pattern = "^sccyto")){
     rm(list = i)
   }
+    
 } else{
   
-  # load in confusion matrix objects from .Rda
-  for(i in list.files(paste0(repo,"analysis/summarystats"), pattern = "ss")){
+  # load in summary objects from .Rda
+  for(i in list.files(paste0(repo,"analysis/summarystats"), pattern = "^ss")){
     load(paste0(repo, "analysis/summarystats/", i))
   }
 }
@@ -96,144 +117,195 @@ for(i in ls(pattern = "^ss.")){
   
   # add temp dataframe of abs/dataset/tool values to list
   tmplist[[i]] <- tempdf
+  
+  # remove temp objects
+  rm(df, end, j, name, start, tempdf)
 }
 
 # collapse list to df, then remove rownames, convert 'coeff' to numeric and
 # change order of columns
 plot.df <- do.call(rbind, tmplist)
 
-# remove problematic Inf row for MTS dataset (ALDEx2, gamma = 0.5)
+# remove problematic Inf rows for MTS/sccyto datasets
+length(which(plot.df$abs == Inf)) # 1 for mts, 28 for sccyto
 plot.df <- plot.df[-grep(Inf, plot.df$abs),]
 
-# split tool into tool and gamma
-plot.df$gamma.char <- as.character(gsub("aldex._", "", plot.df$tool))
-plot.df$gamma.num <- as.numeric(gsub("aldex._", "", plot.df$tool))
+# split tool into tool and gamma (character and numeric)
+plot.df$gamma.char <- as.character(gsub("aldex._", "0.", plot.df$tool))
+plot.df$gamma.char <- as.character(gsub("0\\.0", "0", plot.df$gamma.char))
+plot.df$gamma.num <- as.numeric(gsub("aldex._", "0.", plot.df$tool))
 plot.df$tool <- gsub("_.$","", plot.df$tool)
 
-plot.immuno <- plot.df %>% 
-  filter(dataset == "immuno")
+# convert tools to correct case
+plot.df$tool <- gsub("aldex","ALDEx", plot.df$tool)
 
+# add 12 colours for scale values: 6 for ALDEx2, 6 for ALDEx3
+#(note for future Scott: you selected these colours by running the following:
+# colorRampPalette(colors = c("lavender", "lightskyblue1", "dodgerblue2", "#2171B5", "#08306B"), space = "rgb")(9))
+# brewer.pal(n = 9, name = "YlOrRd")
+# and choosing the ones with the most difference between them
+cols.density <- c("#E6E6FA","#CBE3FC","#B0E2FF","#66B3F6","#2171B5","#08306B",
+                  brewer.pal(n = 9, name = "YlOrRd")[c(1,3,5,6,8,9)])
+
+"#FFFFCC" "#FED976" "#FD8D3C" "#FC4E2A" "#BD0026" "#800026"
 
 #################### Density Plot Panel ######################
 
-group_means <- plot.immuno %>%
+# filter for immuno dataset
+plot.filter <- plot.df %>% 
+  filter(dataset == "immuno")
+
+# add colour values to filtered dataset
+plot.filter$denscol <- case_when(plot.filter$gamma.char == "0" & plot.filter$tool == "ALDEx2" ~ cols.density[1],
+                                 plot.filter$gamma.char == "0.1" & plot.filter$tool == "ALDEx2" ~ cols.density[2],
+                                 plot.filter$gamma.char == "0.2" & plot.filter$tool == "ALDEx2" ~ cols.density[3],
+                                 plot.filter$gamma.char == "0.3" & plot.filter$tool == "ALDEx2" ~ cols.density[4],
+                                 plot.filter$gamma.char == "0.4" & plot.filter$tool == "ALDEx2" ~ cols.density[5],
+                                 plot.filter$gamma.char == "0.5" & plot.filter$tool == "ALDEx2" ~ cols.density[6],
+                                 plot.filter$gamma.char == "0" & plot.filter$tool == "ALDEx3" ~ cols.density[7],
+                                 plot.filter$gamma.char == "0.1" & plot.filter$tool == "ALDEx3" ~ cols.density[8],
+                                 plot.filter$gamma.char == "0.2" & plot.filter$tool == "ALDEx3" ~ cols.density[9],
+                                 plot.filter$gamma.char == "0.3" & plot.filter$tool == "ALDEx3" ~ cols.density[10],
+                                 plot.filter$gamma.char == "0.4" & plot.filter$tool == "ALDEx3" ~ cols.density[11],
+                                 plot.filter$gamma.char == "0.5" & plot.filter$tool == "ALDEx3" ~ cols.density[12])
+
+# convert colours to factor based on tool & gamma order
+plot.filter$denscol <- factor(plot.filter$denscol, levels = cols.density)
+
+# make vector of scale values for legend
+leg.vals <- c(paste0("ALDEx2: ", c(0,0.1,0.2,0.3,0.4,0.5)),
+              paste0("ALDEx3: ", c(0,0.1,0.2,0.3,0.4,0.5)))
+
+
+# get group means for given dataset and add colours
+group_means.filter<- plot.filter %>%
   group_by(tool, gamma.char) %>%
-  summarise(mean_value = mean(abs), .groups='drop') #####
+  summarise(mean_value = mean(abs))
 
-group_means0 <- plot.df %>%
-  group_by(tool, dataset, gamma.num) %>%
-  summarise(mean_value = mean(abs), .groups='drop') #####
+group_means.filter$linecols <- cols.density
 
-p1<-plot.immuno %>% 
-  ggplot(aes(x = abs, fill=gamma.char)) + 
-  geom_density(alpha=0.5)+
-  geom_vline(data = group_means, aes(xintercept = mean_value, colour=gamma.char), linetype="dashed", linewidth=0.5) +
+# plot example density plot for given dataset
+# png(paste0(repo,"figures/fig3_densityPlotsALDEx.png"),
+#     units = "in", height = 4, width = 6, res = 600)
+
+p1 <- plot.filter %>% 
+  ggplot(aes(x = abs)) + 
+  geom_density(aes(fill = denscol), linewidth = 0.3, alpha=0.7)+
+  scale_fill_identity(name = "Scale (\u03b3)", guide = "legend", labels = leg.vals)+
+  geom_vline(data = group_means.filter, aes(xintercept = mean_value, colour=linecols), linetype="dashed", linewidth=0.3) +
+  scale_colour_identity()+
+  xlab('Minimum absolute log difference for significance')+ ylab('Density')+
+  scale_x_continuous(limits = c(0,1.99), expand = c(0,0))+
+  scale_y_continuous(limits = c(0,11), expand = c(0,0))+
   theme_bw()+
   facet_wrap(~tool)+
-  xlim(0,2)+
-  labs(title = "Immunotherapy Minimum Difference Density Distribution")+
-  xlab('Minimum Absolute Difference for Significance')+
-  ylab('Density')
+  theme(axis.text = element_text(size = 7), axis.title = element_text(size = 8, face = "bold"),
+        strip.text = element_text(face = "bold"), legend.box.spacing = unit(0.15, "cm"),
+        legend.text = element_text(size = 6), legend.title = element_text(size = 7, face = "bold"),
+        legend.key.spacing.y = unit(0.1, "cm"), legend.key.spacing.x = unit(0, "cm"),
+        legend.key.size = unit(0.5, "cm"), legend.margin = margin(0,0,0,0.0,"cm"))
 
+p1
 
-p2 <- ggplot(group_means0, aes(x = gamma.num, y = mean_value, colour=dataset, shape = tool)) +
-  geom_jitter(width = 0.01, alpha = 0.5) + 
-  labs(title = "Average Minimum Difference Across Datasets",
-       x = "Scale",
-       y = "Mean Absolute Minimum Difference for Significance") +
+# dev.off()
+
+# calculate group means for ALL datasets, then filter for all human-derived, 
+# bulk transcriptome datasets, and other datasets, separately
+group_means.all <- plot.df %>%
+  group_by(tool, dataset, gamma.num) %>%
+  summarise(mean_value = mean(abs))
+
+# also add column ensuring these data points are represented in legend
+group_means.hum <- group_means.all %>% 
+  filter(dataset %in% c("brca","immuno","kirc","lihc","luad","prad","thca")) %>% 
+  mutate(shp = "TCGA transcriptomes")
+
+# also add column for strip title and replace dataset abbreviations with full
+# labels
+group_means.oth <- group_means.all %>% 
+  filter(!dataset %in% c("brca","immuno","kirc","lihc","luad","prad","thca")) %>% 
+  mutate(title = "Minimum absolute difference between groups for P <0.05")
+
+group_means.oth$dataset <- case_when(group_means.oth$dataset == "mts" ~ "Vaginal metatranscriptome",
+                                     group_means.oth$dataset == "sccyto" ~ "Single-cell transcriptome",
+                                     group_means.oth$dataset == "yeast" ~ "Yeast transcriptome")
+
+# plot stripchart: colours = tool
+png(paste0(repo,"figures/fig3_stripchartToolALDEx.png"),
+    units = "in", height = 4, width = 6, res = 600)
+
+p2 <- ggplot(group_means.oth, aes(x = gamma.num, y = mean_value, colour=tool, shape = dataset))+
+  geom_point(alpha = 1, size = 1)+
+  geom_line(linewidth = 0.25)+
+  scale_colour_manual(name = "Tool", values = c("dodgerblue2", "orangered2"))+
+  stat_summary(data = group_means.hum, aes(group = tool), geom = "errorbar",
+               fun.data = "mean_se", width = 0.0075, linewidth = 0.35, colour = "black")+
+  stat_summary(data = group_means.hum, aes(group = tool, shape = shp),
+               geom = "point", fun = "mean", size = 1.5)+
+  stat_summary(data = group_means.hum, aes(group = tool),
+               geom = "line", fun = "mean", linewidth = 0.25)+
+  scale_x_continuous(limits = c(-0.005, 0.505), expand = c(0.0025,0.01))+
+  scale_shape_manual(name = "Dataset",
+                     values = c("Vaginal metatranscriptome" = 16, "Single-cell transcriptome" = 17,
+                                "Yeast transcriptome" = 15, "TCGA transcriptomes" = 18))+
+  labs(x = "Scale (\u03b3)", y = "Minimum log difference")+
   theme_bw()+
-  geom_point(size = 2.5)
+  facet_wrap(~title)+
+  theme(axis.title = element_text(size = 8), axis.text = element_text(size = 7),
+        legend.key.spacing.y = unit(0.1, "cm"), legend.key.spacing.x = unit(0, "cm"),
+        legend.key.size = unit(0.1, "cm"), legend.margin = margin(0,0,0,0.0,"cm"),
+        legend.title = element_text(face = "bold", size = 7), legend.text = element_text(size = 6),
+        legend.box.spacing = unit(0.2, "cm"), strip.text = element_text(face = "bold"))
 
-p1|p2 #to plot as a 2 panel figure
+p2
 
-################# volcano plots ######################
-
-load(paste(anal.path,"immuno.data.aldex2_0.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex2_1.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex2_2.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex2_5.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex3_0.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex3_1.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex3_2.Rda", sep=""))
-load(paste(anal.path,"immuno.data.aldex3_5.Rda", sep=""))
-
-# Convert results to data frames
-df1 <- as.data.frame(immuno.data_0.aldex2$t.data[[30]])
-df2 <- as.data.frame(immuno.data_1.aldex2$t.data[[30]])
-df3 <- as.data.frame(immuno.data_2.aldex2$t.data[[30]])
-df4 <- as.data.frame(immuno.data_5.aldex2$t.data[[30]])
-
-df1$scale <- "0"
-df2$scale <- "0.1"  
-df3$scale <- "0.2"
-df4$scale <- "0.5" 
-
-df_combined <- bind_rows(df1, df2, df3, df4)
-
-# Volcano plot overlay
-p3<- ggplot(df_combined, aes(x = diff.btw, y = -log10(we.eBH), color = scale)) +
-       geom_point(alpha = 0.6) +
-       geom_hline(yintercept = -log10(0.05), linetype = "dashed") +  # FDR threshold
-       theme_bw() +
-       labs(title = "ALDEx2 Volcano Plot",
-            x = "diff.btw",
-            y = "-log10 Adjusted we.eBH",
-            color = "scale")
-
-# Convert results to data frames
-df5 <- as.data.frame(immuno.data_0.aldex3$t.data[[30]])
-df6 <- as.data.frame(immuno.data_1.aldex3$t.data[[30]])
-df7 <- as.data.frame(immuno.data_2.aldex3$t.data[[30]])
-df8 <- as.data.frame(immuno.data_5.aldex3$t.data[[30]])
-
-df5$scale <- "0"
-df6$scale <- "0.1"  
-df7$scale <- "0.2"
-df8$scale <- "0.5" 
-
-df_combined2 <- bind_rows(df5, df6, df7, df8)
-
-# Volcano plot overlay
-p4<- ggplot(df_combined2, aes(x = estimate, y = -log10(p.val.adj), color = scale)) +
-  geom_point(alpha = 0.6) +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed") +  # FDR threshold
-  theme_bw() +
-  labs(title = "ALDEx3 Volcano Plot",
-       x = "estimate",
-       y = "-log10 Adjusted p.val.adj",
-       color = "scale")
+dev.off()
 
 
-p3|p4 #to plot as 2 panel figure
+# plot stripchart: colours = dataset
+png(paste0(repo,"figures/fig3_stripchartDatasetALDEx.png"),
+    units = "in", height = 4, width = 6, res = 600)
 
-#################### effect size plots #######################
+p3 <- ggplot(group_means.oth, aes(x = gamma.num, y = mean_value, colour=dataset, shape = tool))+
+  geom_point(alpha = 1, size = 1)+
+  geom_line(linewidth = 0.25)+
+  labs(x = "Scale (\u03b3)", y = "Minimum log difference")+
+  stat_summary(data = group_means.hum, aes(group = tool), geom = "errorbar",
+               fun.data = "mean_se", width = 0.0075, linewidth = 0.35, colour = "black")+
+  stat_summary(data = group_means.hum, aes(group = tool,  colour = shp),
+               geom = "point", fun = "mean", size = 1)+
+  stat_summary(data = group_means.hum, aes(group = tool), colour = "purple3",
+               geom = "line", fun = "mean", linewidth = 0.25)+
+  scale_x_continuous(limits = c(-0.005, 0.505), expand = c(0.0025,0.01))+
+  scale_colour_manual(name = "Dataset",
+                      values = c("Vaginal metatranscriptome" = "dodgerblue", "Single-cell transcriptome" = "orangered2",
+                                 "Yeast transcriptome" = "goldenrod2", "TCGA transcriptomes" = "purple3"))+
+  labs(x = "Scale (\u03b3)", y = "Minimum log difference", shape = "Tool")+
+  theme_bw()+
+  facet_wrap(~title)+
+  theme(axis.title = element_text(size = 8), axis.text = element_text(size = 7),
+        legend.key.spacing.y = unit(0.1, "cm"), legend.key.spacing.x = unit(0, "cm"),
+        legend.key.size = unit(0.1, "cm"), legend.margin = margin(0,0,0,0.0,"cm"),
+        legend.title = element_text(face = "bold", size = 7), legend.text = element_text(size = 6),
+        legend.box.spacing = unit(0.2, "cm"), strip.text = element_text(face = "bold"))
 
-#aldex2 panel
-p5<-ggplot(df_combined, aes(x = diff.win, y = diff.btw, color = scale, shape = we.eBH < 0.05)) +
-geom_point(alpha = 0.6, size = 1) +
-geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "black") +  
-geom_abline(slope = -1, intercept = 0, linetype = "dashed", color = "black") +
-theme_bw() +
-labs(title = "ALDEx2 Effect Plot",
-     x = "diff.win",
-     y = "diff.btw",
-     color = "scale")
+p3
 
-p6<-ggplot(df_combined2, aes(x = std.error*sqrt(109), y = estimate, color = scale)) + #need to change this based on what corresponds to diff.win and diff.btw for aldex3
-  geom_point(alpha = 0.6, size = 0.8) +
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "black") +  
-  geom_abline(slope = -1, intercept = 0, linetype = "dashed", color = "black") +
-  theme_bw() +
-  labs(title = "ALDEx3 Effect Plot",
-       x = "std.error*sqrt(number of samples)",
-       y = "estimate",
-       color = "scale")
+dev.off()
 
-p5|p6
+# plot density plots and stripchart as a two-panel figure (strip = tool)
+png(paste0(repo,"figures/fig3_bothTool.png"),
+    units = "in", height = 4, width = 10, res = 600)
+
+p1|p2
+
+dev.off()
 
 
+# plot density plots and stripchart as a two-panel figure (strip = dataset)
+png(paste0(repo,"figures/fig3_bothDataset.png"),
+    units = "in", height = 4, width = 10, res = 600)
 
-#plot(immuno.data_0.aldex3$t.data[[1]]$std.error*sqrt(120), immuno.data_0.aldex3$t.data[[1]]$estimate)
-#std.error * sqrt(total number of samples) <- diff.win
-#estimate <-  diff.btw
+p1|p3
 
+dev.off()
