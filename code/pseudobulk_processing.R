b65f6431262f03bc76955fa9037847d4ac0c1515
scottdossantos <136380707+scottdossantos@users.noreply.github.com>
2026-07-21 12:39:04 -0400

    code for pseudobulked RNA-seq processing and analysis
    - Added code for processing pseudobulked scRNA-dataset (an example dataset used by the popular 'seurat' R package for one of their vignettes). NOTE: confirmed that seurat aggregation and manual aggregation give the same output.
    
    - Added code for using the output of the pseudobulk processing and running through all analysis tools, for 100 iterations.
    
    - Removed GG's initial pseudobulking code

--- code/pseudobulk_processing.R
+++ code/pseudobulk_processing.R
@@ -2,7 +2,7 @@
 # scale benchmarking
 
 # Greg Gloor & Scott Dos Santos
-# Last edited: 30th June 2026
+# Last edited: 21st July 2026
 
 # This code generates a pseudo-bulk RNA-seq dataset from single cell dataset
 # found as an exemplar in the Seurat R package for scRNA-seq data. This dataset
@@ -19,22 +19,26 @@
 #################################### setup ####################################
 
 library(Seurat)
-library(SeuratData)
 library(Matrix)
+library(SeuratData) # if loading from cloned github repo, ignore & use devtools
 
 # clone the seurat-data repo (https://github.com/satijalab/seurat-data) and then
 # load via devtools
 # devtools::load_all("~/Documents/GitHub/seurat-data/")
 
-# check available seurat datasets
-AvailableData()
+# check available seurat datasets and install if missing, otherwise, load the
+# ifnb dataset (ignore the warnings!)
+if(AvailableData()["ifnb.SeuratData","Installed"] == FALSE){
+  AvailableData()
+  
+  InstallData('ifnb')
+} else{
+  
+  ifnb.orig <- LoadData("ifnb")
+  ifnb <- ifnb.orig
+  
+}
 
-# install the ifnb dataset; ONLY NEEDS TO BE RUN ONCE!
-# InstallData('ifnb')
-
-# load the ifnb dataset (ignore warnings)
-ifnb.orig <- LoadData("ifnb")
-ifnb <- ifnb.orig
 # load the inferred sample IDs of each cell from the Kang dataset github repo
 # (origin: https://github.com/yelabucsf/demuxlet_paper_code/tree/master/fig3 but
 # these files have been saved in their original form in the current study repo)
@@ -72,26 +76,6 @@ ifnb <- AddMetaData(ifnb, metadata = info)
 ifnb$donor_id[is.na(ifnb$donor_id)] <- "unknown"
 ifnb <- subset(ifnb, subset = donor_id != "unknown")
 
-##### added by GG July 16, 2026
-# get metadata
-meta <- ifnb@meta.data
-# make a master vector of unique names STIM_DONOR_TYPE to use for aggregation
-# substitute spaces with _ in seurat_annotation column
-meta$seurat_annotations <- gsub(" ", "_", meta$seurat_annotations)
-# remove SNG- from the Donor names
-meta$donor_id <- gsub("SNG-", "", meta$donor_id)
-# concatenate names
-con_names <- paste(meta$stim, meta$donor_id, meta$seurat_annotations, sep="_")
-
-counts <- as.data.frame(Matrix(ifnb$RNA$counts, sparse=F))
-
-# finally aggregate 
-ifnb.agg <- stats::aggregate(t(counts), by=list(con_names), FUN=sum)
-rownames(ifnb.agg) <- ifnb.agg$Group.1
-ifnb.agg$Group.1 <- NULL
-####
-
-# subset into  
 # pseudobulk the counts based on donor-condition-celltype
 pseudo_ifnb <- AggregateExpression(ifnb, assays = "RNA", return.seurat = T, 
                                    group.by = c("stim", "donor_id", "seurat_annotations"))
@@ -103,18 +87,57 @@ Idents(pseudo_ifnb) <- "celltype.stim"
 # export normalised/aggregated count data to matrix
 seu.agg.counts <- as.data.frame(Matrix(pseudo_ifnb$RNA$counts,sparse=F))
 
-#### added by GG July 16, 2026
-#  head(colSums(seu.agg.counts), n=10L)
-#  head(colSums(ifnb.agg), n=10L)
-# tldr: equivalent columns give exactly the same read count total and 
-# the same count per gene
-####
-
 # move rownames to own column and move to front of df
-counts$feature <- rownames(counts)
-counts <- counts[,c(ncol(counts), 1:ncol(counts)-1)]
+seu.agg.counts$feature <- rownames(seu.agg.counts)
+seu.agg.counts <- seu.agg.counts[,c(ncol(seu.agg.counts), 1:ncol(seu.agg.counts)-1)]
+
+# replace spaces with underscores in column names
+colnames(seu.agg.counts) <- gsub(" ", "_", colnames(seu.agg.counts))
 
 # write to txt
 write.table(counts, file = paste0("~/Documents/GitHub/usri/data/pseudobulk_counts.txt"),
             sep = "\t", col.names = T, row.names = F, quote = F)
 
+############################## check aggregation ##############################
+
+# want to check that seurat's aggregation function is *ACTUALLY* returning raw
+# counts instead of some sort of normalised counts. Manually aggregate counts
+
+# re-process raw data before CLR normalisation
+ifnb.manual <- ifnb.orig
+ifnb.manual <- AddMetaData(ifnb.manual, metadata = info)
+ifnb.manual$donor_id[is.na(ifnb.manual$donor_id)] <- "unknown"
+ifnb.manual <- subset(ifnb.manual, subset = donor_id != "unknown")
+
+# get metadata from seurat object
+meta <- ifnb.manual@meta.data
+
+# make a master vector of unique names following pattern of 'GROUP_DONOR_TYPE'
+# to use for aggregation: start by substituting spaces with underscores in the
+# 'seurat_annotation' column
+meta$seurat_annotations <- gsub(" ", "_", meta$seurat_annotations)
+
+# add back in the dash betwen 'SNG' and donor id, then concatenate sample names
+meta$donor_id <- gsub("SNG_", "SNG-", meta$donor_id)
+con_names <- paste(meta$stim, meta$donor_id, meta$seurat_annotations, sep="_")
+
+# export as matrix and aggregate raw counts manually (takes a little while)
+counts <- as.data.frame(Matrix(ifnb$RNA$counts, sparse=F))
+ifnb.manual.agg <- stats::aggregate(t(counts), by=list(con_names), FUN=sum)
+
+# move aggregated sample names from their own column to row names
+rownames(ifnb.manual.agg) <- ifnb.manual.agg$Group.1
+ifnb.manual.agg$Group.1 <- NULL
+
+# transpose to get samples as columns and re-order based on seurat-aggregated
+# data frame column names (excluding 'feature column')
+ifnb.manual.agg <- as.data.frame(t(ifnb.manual.agg))
+ifnb.manual.agg <- ifnb.manual.agg[,colnames(seu.agg.counts[2:207])]
+
+# are the sums of each sample identical?
+all(colSums(ifnb.manual.agg)==colSums(seu.agg.counts[,2:ncol(seu.agg.counts)])) # returns TRUE
+
+# are the data frames identical?
+all(ifnb.manual.agg == seu.agg.counts[,2:ncol(seu.agg.counts)]) # returns TRUE
+
+# samples give exactly the same read counts per sample AND per gene
